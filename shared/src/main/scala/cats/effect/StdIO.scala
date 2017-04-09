008765042c19b83cc56c0a063ebb55562c7a5e18
Daniel Spiewak <djspiewak@gmail.com>
2017-04-08 23:05:57 -0600

    Added some cool std things

---
+++ shared/src/main/scala/cats/effect/StdIO.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 Daniel Spiewak
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats
+package effect
+
+import scala.io.StdIn
+
+object StdIO {
+
+  def println(str: String): IO[Unit] = IO { Console.println(str) }
+
+  def print[A: Show](a: A): IO[Unit] = IO { Console.print(Show[A].show(a)) }
+
+  val readLine: IO[String] = IO { StdIn.readLine }
+
+  def exit(code: Int): IO[Nothing] = IO { System.exit(code); sys.error("unreachable") }
+
+  val availableProcessors: IO[Int] = IO { Runtime.getRuntime.availableProcessors }
+
+  val shutdownHook: IO[IO[Unit]] = IO {
+    IO async { cb =>
+      Runtime.getRuntime.addShutdownHook(new Thread {
+        override def run() = {
+          cb(Right(()))
+        }
+      })
+    }
+  }
+}
