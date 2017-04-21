f3460256bcd29382e28dc23844cc554d68563063
Alexandru Nedelcu <noreply@alexn.org>
2017-04-21 17:11:14 -0500

    Improve the JVM blocking in IOPlatform

--- core/jvm/src/main/scala/cats/effect/IOPlatform.scala
+++ core/jvm/src/main/scala/cats/effect/IOPlatform.scala
@@ -17,28 +17,57 @@
 package cats
 package effect
 
-import scala.concurrent.duration.Duration
+import java.util.concurrent.locks.AbstractQueuedSynchronizer
+import scala.concurrent.blocking
+import scala.concurrent.duration.{Duration, FiniteDuration}
 import scala.util.Either
 
-import java.util.concurrent.{CountDownLatch, TimeUnit}
-import java.util.concurrent.atomic.AtomicReference
-
 private[effect] object IOPlatform {
-
+  /** 
+   * JVM-specific function that blocks for the result of an IO task.
+   *
+   * Uses the [[scala.concurrent.BlockContext]], instructing the
+   * underlying thread-pool that this is a blocking operation, thus
+   * allowing for defensive measures, like adding more threads or
+   * executing other queued tasks first.
+   */
   def unsafeResync[A](ioa: IO[A], limit: Duration): Option[A] = {
-    val latch = new CountDownLatch(1)
-    val ref = new AtomicReference[Either[Throwable, A]](null)
+    val latch = new OneShotLatch
+    var ref: Either[Throwable, A] = null
 
     ioa unsafeRunAsync { a =>
-      ref.set(a)
-      latch.countDown()
+      // Reading from `ref` happens after the block on `latch` is
+      // over, there's a happens-before relationship, so no extra
+      // synchronization is needed for visibility
+      ref = a
+      latch.releaseShared(1)
     }
 
-    if (limit == Duration.Inf)
-      latch.await()
-    else
-      latch.await(limit.toMillis, TimeUnit.MILLISECONDS)
+    limit match {
+      case e if e eq Duration.Undefined =>
+        throw new IllegalArgumentException("Cannot wait for Undefined period")
+      case Duration.Inf =>
+        blocking(latch.acquireSharedInterruptibly(1))
+      case f: FiniteDuration if f > Duration.Zero =>
+        blocking(latch.tryAcquireSharedNanos(1, f.toNanos))
+      case _ =>
+        () // Do nothing
+    }
 
-    Option(ref.get()).map(_.fold(throw _, a => a))
+    ref match {
+      case null => None
+      case Right(a) => Some(a)
+      case Left(ex) => throw ex
+    }
+  }
+
+  private final class OneShotLatch extends AbstractQueuedSynchronizer {
+    override protected def tryAcquireShared(ignored: Int): Int =
+      if (getState != 0) 1 else -1
+
+    override protected def tryReleaseShared(ignore: Int): Boolean = {
+      setState(1)
+      true
+    }
   }
 }
