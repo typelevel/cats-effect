eecdf49d030cc099ef16082b902d9919c84be18f
Alexandru Nedelcu <noreply@alexn.org>
2018-01-12 20:08:44 +0200

    Add internal `parMap2` implementation

--- core/shared/src/main/scala/cats/effect/internals/IOParMap.scala
+++ core/shared/src/main/scala/cats/effect/internals/IOParMap.scala
@@ -16,10 +16,64 @@
 
 package cats.effect.internals
 
+import java.util.concurrent.atomic.AtomicReference
 import cats.effect.IO
+import scala.concurrent.ExecutionContext
 
 private[effect] object IOParMap {
+  type Attempt[A] = Either[Throwable, A]
+  type State[A, B] = Either[Attempt[A], Attempt[B]]
 
   def apply[A, B, C](fa: IO[A], fb: IO[B])(f: (A, B) => C): IO[C] =
-    IO.async { cb => }
+    IO.async { cb =>
+      // For preventing stack-overflow errors; using a
+      // trampolined execution context, so no thread forks
+      private implicit val ec: ExecutionContext = TrampolineEC.immediate
+
+      // Light async boundary to prevent SO errors
+      ec.execute(new Runnable {
+        def run(): Unit = {
+          val state = new AtomicReference[State[A, B]]()
+
+          def complete(ra: Attempt[A], rb: Attempt[B]): Unit =
+            // Second async boundary needed just before the callback
+            ec.execute(new Runnable {
+              def run(): Unit = ra match {
+                case Right(a) =>
+                  rb match {
+                    case Right(b) =>
+                      cb(try Right(f(a, b)) catch { case NonFatal(e) => Left(e) })
+                    case error @ Left(_) =>
+                      cb(error)
+                  }
+                case error @ Left(_) =>
+                  cb(error)
+                  rb match {
+                    case Left(error2) => throw error2
+                    case _ => ()
+                  }
+              }
+            })
+
+          // First execution
+          fa.unsafeRunAsync { attemptA =>
+            // Using Java 8 platform intrinsics
+            state.getAndSet(Left(attemptA)) match {
+              case null => () // wait for B
+              case Right(attemptB) => complete(attemptA, attemptB)
+              case left => throw new IllegalStateException(s"parMap: $left")
+            }
+          }
+          // Second execution
+          fb.unsafeRunAsync { attemptB =>
+            // Using Java 8 platform intrinsics
+            state.getAndSet(Right(attemptB)) match {
+              case null => () // wait for B
+              case Left(attemptA) => complete(attemptA, attemptB)
+              case right => throw new IllegalStateException(s"parMap: $right")
+            }
+          }
+        }
+      })
+    }
 }
