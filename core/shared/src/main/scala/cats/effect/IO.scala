a91c9d8888ae9848f8696508618bed3fb2a8103f
Alexandru Nedelcu <noreply@alexn.org>
2018-01-17 10:33:09 +0200

    Fix scaladoc

--- core/shared/src/main/scala/cats/effect/IO.scala
+++ core/shared/src/main/scala/cats/effect/IO.scala
@@ -287,7 +287,7 @@ sealed abstract class IO[+A] {
 
 private[effect] abstract class IOParallelNewtype {
   /** Newtype encoding for an `IO` datatype that has a `cats.Applicative`
-    * capable of doing parallel processing in ap` and `map2`, needed
+    * capable of doing parallel processing in `ap` and `map2`, needed
     * for implementing `cats.Parallel`.
     *
     * Helpers are provided as extension methods for converting back and forth
