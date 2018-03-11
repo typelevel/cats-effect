9e715a7d243f047bde922bae27398409a858f314
Alexandru Nedelcu <noreply@alexn.org>
2018-03-11 19:12:20 +0200

    Fix comment

--- core/shared/src/main/scala/cats/effect/internals/AndThen.scala
+++ core/shared/src/main/scala/cats/effect/internals/AndThen.scala
@@ -31,7 +31,7 @@ import java.io.Serializable
  * Example:
  *
  * {{{
- *   val seed = AndThen((x: Int) => x + 1))
+ *   val seed = AndThen.of((x: Int) => x + 1))
  *   val f = (0 until 10000).foldLeft(seed)((acc, _) => acc.andThen(_ + 1))
  *   // This should not trigger stack overflow ;-)
  *   f(0)
