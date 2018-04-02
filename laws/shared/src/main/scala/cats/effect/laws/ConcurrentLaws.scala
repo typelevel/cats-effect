5c714f68bc2d38a4061f5335c6acf9115fafe06b
Aki Legaspi <aki@tirsolutions.com>
2018-04-02 09:19:38 +0800

    Changed `Async$.never` to F.never in ConcurrentLaw
    Changed the helper function to F.never since the deprecation of old
    method

--- laws/shared/src/main/scala/cats/effect/laws/ConcurrentLaws.scala
+++ laws/shared/src/main/scala/cats/effect/laws/ConcurrentLaws.scala
@@ -91,11 +91,11 @@ trait ConcurrentLaws[F[_]] extends AsyncLaws[F] {
   }
 
   def raceMirrorsLeftWinner[A](fa: F[A], default: A) = {
-    F.race(fa, Async.never[F, A]).map(_.left.getOrElse(default)) <-> fa
+    F.race(fa, F.never[A]).map(_.left.getOrElse(default)) <-> fa
   }
 
   def raceMirrorsRightWinner[A](fa: F[A], default: A) = {
-    F.race(Async.never[F, A], fa).map(_.right.getOrElse(default)) <-> fa
+    F.race(F.never[A], fa).map(_.right.getOrElse(default)) <-> fa
   }
 
   def raceCancelsLoser[A, B](r: Either[Throwable, A], leftWinner: Boolean, b: B) = {
