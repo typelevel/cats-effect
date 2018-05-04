2c1748450088a52125158d2cc16d067bc1f19f12
Michael Pilquist <mpilquist@gmail.com>
2018-05-03 20:44:19 -0400

    Updated Ref modify variants for common use cases

--- core/shared/src/main/scala/cats/effect/concurrent/Ref.scala
+++ core/shared/src/main/scala/cats/effect/concurrent/Ref.scala
@@ -31,58 +31,58 @@ import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
 import scala.annotation.tailrec
 
 /**
-  * An asynchronous, concurrent mutable reference.
-  *
-  * Provides safe concurrent access and modification of its content, but no
-  * functionality for synchronisation, which is instead handled by [[Promise]].
-  * For this reason, a `Ref` is always initialised to a value.
-  *
-  * The implementation is nonblocking and lightweight, consisting essentially of
-  * a purely functional wrapper over an `AtomicReference`.
-  */
+ * An asynchronous, concurrent mutable reference.
+ *
+ * Provides safe concurrent access and modification of its content, but no
+ * functionality for synchronisation, which is instead handled by [[Promise]].
+ * For this reason, a `Ref` is always initialised to a value.
+ *
+ * The implementation is nonblocking and lightweight, consisting essentially of
+ * a purely functional wrapper over an `AtomicReference`.
+ */
 final class Ref[F[_], A] private (private val ar: AtomicReference[A]) {
 
   /**
-    * Obtains the current value.
-    *
-    * Since `Ref` is always guaranteed to have a value, the returned action
-    * completes immediately after being bound.
-    */
+   * Obtains the current value.
+   *
+   * Since `Ref` is always guaranteed to have a value, the returned action
+   * completes immediately after being bound.
+   */
   def get(implicit F: Sync[F]): F[A] = F.delay(ar.get)
 
   /**
-    * Synchronously sets the current value to `a`.
-    *
-    * The returned action completes after the reference has been successfully set.
-    *
-    * Satisfies:
-    *   `r.set(fa) *> r.get == fa`
-    */
+   * Synchronously sets the current value to `a`.
+   *
+   * The returned action completes after the reference has been successfully set.
+   *
+   * Satisfies:
+   *   `r.set(fa) *> r.get == fa`
+   */
   def set(a: A)(implicit F: Sync[F]): F[Unit] = F.delay(ar.set(a))
 
   /**
-    * Lazily sets the current value to the `a`.
-    *
-    * After the returned `F[Unit]` is bound, an update will eventually occur,
-    * setting the current value to `a`.
-    *
-    * Satisfies:
-    *   `r.lazySet(fa) == async.shiftStart(r.set(a))`
-    * but it's significantly faster.
-    */
+   * Lazily sets the current value to the `a`.
+   *
+   * After the returned `F[Unit]` is bound, an update will eventually occur,
+   * setting the current value to `a`.
+   *
+   * Satisfies:
+   *   `r.lazySet(fa) == async.shiftStart(r.set(a))`
+   * but it's significantly faster.
+   */
   def lazySet(a: A)(implicit F: Sync[F]): F[Unit] = F.delay(ar.lazySet(a))
 
   /**
-    * Obtains a snapshot of the current value, and a setter for updating it.
-    * The setter may noop (in which case `false` is returned) if another concurrent
-    * call to `access` uses its setter first.
-    *
-    * Once it has noop'd or been used once, a setter never succeeds again.
-    *
-    * Satisfies:
-    *   `r.access.map(_._1) == r.get`
-    *   `r.access.flatMap { case (v, setter) => setter(f(v)) } == r.tryModify(f).map(_.isDefined)`
-    */
+   * Obtains a snapshot of the current value, and a setter for updating it.
+   * The setter may noop (in which case `false` is returned) if another concurrent
+   * call to `access` uses its setter first.
+   *
+   * Once it has noop'd or been used once, a setter never succeeds again.
+   *
+   * Satisfies:
+   *   `r.access.map(_._1) == r.get`
+   *   `r.access.flatMap { case (v, setter) => setter(f(v)) } == r.tryModify(f).map(_.isDefined)`
+   */
   def access(implicit F: Sync[F]): F[(A, A => F[Boolean])] = F.delay {
     val snapshot = ar.get
     val hasBeenCalled = new AtomicBoolean(false)
@@ -91,24 +91,41 @@ final class Ref[F[_], A] private (private val ar: AtomicReference[A]) {
   }
 
   /**
-    * Attempts to modify the current value once, returning `None` if another
-    * concurrent modification completes between the time the variable is
-    * read and the time it is set.
-    */
-  def tryModify[B](f: A => (A, B))(implicit F: Sync[F]): F[Option[B]] = F.delay {
+   * Attempts to modify the current value once, returning `false` if another
+   * concurrent modification completes between the time the variable is
+   * read and the time it is set.
+   */
+  def tryModify(f: A => A)(implicit F: Sync[F]): F[Boolean] =
+    F.map(tryModifyAndReturn(a => (a, ())))(_.isDefined)
+
+  /**
+   * Like `tryModify` but allows the update function to return an output value of
+   * type `B`. The returned action completes with `None` if the value is not updated
+   * successfully and `Some(b)` otherwise.
+   */
+  def tryModifyAndReturn[B](f: A => (A, B))(implicit F: Sync[F]): F[Option[B]] = F.delay {
     val c = ar.get
     val (u, b) = f(c)
     if (ar.compareAndSet(c, u)) Some(b)
     else None
   }
 
+
   /**
-    * Like `tryModify` but does not complete until the update has been successfully made.
-    *
-    * Satisfies:
-    *   `r.modify(_ => (a, ())).void == r.set(a)`
-    */
-  def modify[B](f: A => (A, B))(implicit F: Sync[F]): F[B] = {
+   * Modifies the current value using the supplied update function. If another modification
+   * occurs between the time the current value is read and subsequently updated, the modification
+   * is retried using the new value. Hence, `f` may be invoked multiple times.
+   *
+   * Satisfies:
+   *   `r.modify(_ => a).void == r.set(a)`
+   */
+  def modify(f: A => A)(implicit F: Sync[F]): F[Unit] =
+    modifyAndReturn(a => (a, ()))
+
+  /**
+   * Like `tryModifyAndReturn` but does not complete until the update has been successfully made.
+   */
+  def modifyAndReturn[B](f: A => (A, B))(implicit F: Sync[F]): F[B] = {
     @tailrec
     def spin: B = {
       val c = ar.get
@@ -127,10 +144,10 @@ object Ref {
     F.delay(unsafeCreate(a))
 
   /**
-    * Like `apply` but returns the newly allocated ref directly instead of wrapping it in `F.delay`.
-    * This method is considered unsafe because it is not referentially transparent -- it allocates
-    * mutable state.
-    */
+   * Like `apply` but returns the newly allocated ref directly instead of wrapping it in `F.delay`.
+   * This method is considered unsafe because it is not referentially transparent -- it allocates
+   * mutable state.
+   */
   def unsafeCreate[F[_], A](a: A): Ref[F, A] =
     new Ref[F, A](new AtomicReference[A](a))
 }
