0bcafca4bfe351abeed2b19e2853b24a5dca95ec
Alexandru Nedelcu <noreply@alexn.org>
2018-08-13 12:04:33 +0300

    Optimize guaranteeCase

--- core/shared/src/main/scala/cats/effect/internals/IOBracket.scala
+++ core/shared/src/main/scala/cats/effect/internals/IOBracket.scala
@@ -32,13 +32,13 @@ private[effect] object IOBracket {
       // cancelled due to executing it via `IORunLoop.start`
       IORunLoop.start[A](acquire, {
         case Right(a) =>
-          val frame = new ReleaseFrame[A, B](a, release, conn)
+          val frame = new BracketReleaseFrame[A, B](a, release, conn)
           val onNext = {
             val fb = try use(a) catch { case NonFatal(e) => IO.raiseError(e) }
             fb.flatMap(frame)
           }
           // Registering our cancelable token ensures that in case
-          // cancelation is detected, `release` gets called
+          // cancellation is detected, `release` gets called
           conn.push(frame.cancel)
           // Actual execution
           IORunLoop.startCancelable(onNext, conn, cb)
@@ -50,35 +50,61 @@ private[effect] object IOBracket {
   }
 
   /**
-   * Implementation for `IO.ensureCase`.
+   * Implementation for `IO.guaranteeCase`.
    */
   def guaranteeCase[A](source: IO[A], release: ExitCase[Throwable] => IO[Unit]): IO[A] = {
-    apply(IO.unit)(_ => source)((_, ec) => release(ec))
+    IO.Async { (conn, cb) =>
+      val frame = new EnsureReleaseFrame[A](release, conn)
+      val onNext = source.flatMap(frame)
+      // Registering our cancelable token ensures that in case
+      // cancellation is detected, `release` gets called
+      conn.push(frame.cancel)
+      // Actual execution
+      IORunLoop.startCancelable(onNext, conn, cb)
+    }
   }
 
-  private final class ReleaseFrame[A, B](
+  private final class BracketReleaseFrame[A, B](
     a: A,
-    release: (A, ExitCase[Throwable]) => IO[Unit],
+    releaseFn: (A, ExitCase[Throwable]) => IO[Unit],
     conn: IOConnection)
+    extends BaseReleaseFrame[A, B](conn) {
+
+    def release(c: ExitCase[Throwable]): CancelToken[IO] =
+      releaseFn(a, c)
+  }
+
+  private final class EnsureReleaseFrame[A](
+    releaseFn: ExitCase[Throwable] => IO[Unit],
+    conn: IOConnection)
+    extends BaseReleaseFrame[Unit, A](conn) {
+
+    def release(c: ExitCase[Throwable]): CancelToken[IO] =
+      releaseFn(c)
+  }
+
+  private abstract class BaseReleaseFrame[A, B](conn: IOConnection)
     extends IOFrame[B, IO[B]] {
 
-    val cancel: CancelToken[IO] =
-      release(a, ExitCase.Canceled).uncancelable
+    def release(c: ExitCase[Throwable]): CancelToken[IO]
 
-    def recover(e: Throwable): IO[B] = {
+    final val cancel: CancelToken[IO] =
+      release(ExitCase.Canceled).uncancelable
+
+    final def recover(e: Throwable): IO[B] = {
       // Unregistering cancel token, otherwise we can have a memory leak;
       // N.B. this piece of logic does not work if IO is auto-cancelable
       conn.pop()
-      release(a, ExitCase.error(e))
+      release(ExitCase.error(e))
         .uncancelable
         .flatMap(new ReleaseRecover(e))
     }
 
-    def apply(b: B): IO[B] = {
+    final def apply(b: B): IO[B] = {
       // Unregistering cancel token, otherwise we can have a memory leak
       // N.B. this piece of logic does not work if IO is auto-cancelable
       conn.pop()
-      release(a, ExitCase.complete)
+      release(ExitCase.complete)
         .uncancelable
         .map(_ => b)
     }
