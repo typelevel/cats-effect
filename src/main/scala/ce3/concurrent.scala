74e0cecf3886bde28346f21156a5d4d821c55308
Daniel Spiewak <djspiewak@gmail.com>
2019-09-15 10:34:54 -0600

    Oh right, not necessarily a bracket

--- src/main/scala/ce3/concurrent.scala
+++ src/main/scala/ce3/concurrent.scala
@@ -43,8 +43,8 @@ trait Concurrent[F[_], E] extends MonadError[F, E] { self: Safe[F, E] =>
   def cancelable[A](fa: F[A]): F[A]
 
   // uncancelable(fa) <-> bracket(fa)(_.pure)(_ => unit)
-  def uncancelable[A](fa: F[A]): F[A] =
-    bracket(fa)(_ => unit[F])(_ => unit[F])
+  // uncancelable(fa) <-> bracket(fa)(_ => unit[F])(_ => unit[F])
+  def uncancelable[A](fa: F[A]): F[A]
 
   // produces an effect which is already canceled (and doesn't introduce an async boundary)
   def canceled[A]: F[A]
