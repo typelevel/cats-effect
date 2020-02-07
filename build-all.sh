93b3f6356d26bbe5adf55b41a2fe00c4c24ef2d8
Daniel Spiewak <djspiewak@gmail.com>
2020-02-07 14:47:39 -0700

    Added build-all convenience script

---
+++ build-all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail # STRICT MODE
+IFS=$'\n\t'       # http://redsymbol.net/articles/unofficial-bash-strict-mode/
+
+for sjs in '1.0.0-RC2' '0.6.32'; do
+  SCALAJS_VERSION=$sjs sbt clean +test
+done
