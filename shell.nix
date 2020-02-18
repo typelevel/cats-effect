2e148e12e057f78fad96efd6a67c9557c4190a0a
Ross A. Baker <ross@rossabaker.com>
2020-02-17 19:14:19 -0500

    Drop niv for inline fetchTarball

--- shell.nix
+++ shell.nix
@@ -1,14 +1,19 @@
-with (import <nixpkgs> {});
 let
-  jekyll = writeScriptBin "jekyll" ''
+  nixpkgs-version = "19.09";
+  pkgs = import (builtins.fetchTarball {
+    name = "nixpkgs-${nixpkgs-version}";
+    url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-version}.tar.gz";
+    sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
+  }) {};
+  jekyll = pkgs.writeScriptBin "jekyll" ''
     ${pkgs.bundler}/bin/bundle && ${pkgs.bundler}/bin/bundle exec jekyll "$@"
   '';
-in stdenv.mkDerivation {
+in pkgs.stdenv.mkDerivation {
   name = "cats-effect";
   buildInputs = [
-    git
     jekyll
-    nodejs
-    sbt
+    pkgs.git
+    pkgs.nodejs
+    pkgs.sbt
   ];
 }
