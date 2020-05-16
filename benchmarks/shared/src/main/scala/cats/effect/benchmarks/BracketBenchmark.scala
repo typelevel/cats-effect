f96e6ad80a50aee1045d417c966b6d3cb76d0ddc
Raas Ahsan <raas.ahsan@gmail.com>
2020-05-15 22:05:58 -0500

    Add BracketBenchmark

---
+++ benchmarks/shared/src/main/scala/cats/effect/benchmarks/BracketBenchmark.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017-2019 The Typelevel Cats-effect Project Developers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cats.effect.benchmarks
+
+import java.util.concurrent.TimeUnit
+
+import cats.effect.IO
+import org.openjdk.jmh.annotations._
+
+/** To do comparative benchmarks between versions:
+ *
+ *     benchmarks/run-benchmark BracketBenchmark
+ *
+ * This will generate results in `benchmarks/results`.
+ *
+ * Or to run the benchmark from within sbt:
+ *
+ *     jmh:run -i 10 -wi 10 -f 2 -t 1 cats.effect.benchmarks.BracketBenchmark
+ *
+ * Which means "10 iterations", "10 warm-up iterations", "2 forks", "1 thread".
+ * Please note that benchmarks should be usually executed at least in
+ * 10 iterations (as a rule of thumb), but more is better.
+ */
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+class BracketBenchmark {
+  @Param(Array("10000"))
+  var size: Int = _
+
+  @Benchmark
+  def happyPath(): Int = {
+    def loop(i: Int): IO[Int] =
+      if (i < size)
+        IO.pure(i)
+          .bracket(i => IO.pure(i + 1))(_ => IO.unit)
+          .flatMap(loop)
+      else
+        IO.pure(i)
+
+    loop(0).unsafeRunSync()
+  }
+
+}
