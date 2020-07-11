afcf01f8ca37616f37ffb454c2a1b948735bed16
Tim Spence <timothywspence@gmail.com>
2020-07-11 23:26:18 +0100

    Rename TrampolineEC tests

--- concurrent/shared/src/test/scala/cats/effect/concurrent/internals/TrampolineECSpec.scala
+++ concurrent/shared/src/test/scala/cats/effect/concurrent/internals/TrampolineECSpec.scala
@@ -14,17 +14,33 @@
  * limitations under the License.
  */
 
+// /*
+//  * Copyright 2020 Typelevel
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *     http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+
 // package cats.effect
 // package internals
 
-// import org.scalatest.matchers.should.Matchers
-// import org.scalatest.funsuite.AnyFunSuite
 // import cats.effect.internals.TrampolineEC.immediate
 // import scala.concurrent.ExecutionContext
 // import cats.effect.internals.IOPlatform.isJVM
 // import scala.collection.immutable.Queue
 
-// class TrampolineECTests extends AnyFunSuite with Matchers with TestUtils {
+// import org.specs2.mutable.Specification
+
+// class TrampolineECTests extends Specification {
 //   implicit val ec: ExecutionContext = immediate
 
 //   def executeImmediate(f: => Unit): Unit =
