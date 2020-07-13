b4291eeb2d666af20f9826ec604e10370d70b540
Tim Spence <timothywspence@gmail.com>
2020-07-13 12:40:44 +0100

    Add spec for timeout

---
+++ core/shared/src/test/scala/cats/effect/TemporalSpec.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2020 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats
+package effect
+package concurrent
+
+import cats.implicits._
+import cats.effect.kernel.Temporal
+
+import scala.concurrent.duration._
+import scala.concurrent.TimeoutException
+
+class TemporalSpec extends BaseSpec { outer =>
+
+  sequential
+
+  "temporal" should {
+    "timeout" should {
+      "succeed" in real {
+        val op = Temporal.timeout(IO.pure(true), 5.millis)
+
+        op.flatMap { res =>
+          IO {
+            res must beTrue
+          }
+        }
+      }
+
+      "cancel a loop" in real {
+        val loop = IO.unit.foreverM
+
+        val op = Temporal.timeout(loop, 5.millis).attempt
+
+        op.flatMap { res =>
+          IO {
+            res must beLike {
+              case Left(e) => e must haveClass[TimeoutException]
+            }
+          }
+        }
+      }
+    }
+
+    "timeoutTo" should {
+      "succeed" in real {
+        val op = Temporal.timeoutTo(IO.pure(true), 5.millis, IO.raiseError(new RuntimeException))
+
+        op.flatMap { res =>
+          IO {
+            res must beTrue
+          }
+        }
+      }
+
+      "use fallback" in real {
+        val loop = IO.unit.foreverM
+
+        val op = Temporal.timeoutTo(loop, 5.millis, IO.pure(true))
+
+        op.flatMap { res =>
+          IO {
+            res must beTrue
+          }
+        }
+      }
+    }
+  }
+}
