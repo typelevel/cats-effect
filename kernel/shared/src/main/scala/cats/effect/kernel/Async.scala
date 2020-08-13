04a7e3423a1716f32cbc2b9aabd1fcd981276502
Tim Spence <timothywspence@gmail.com>
2020-08-13 09:26:16 +0100

    Async for OptionT

--- kernel/shared/src/main/scala/cats/effect/kernel/Async.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/Async.scala
@@ -17,6 +17,7 @@
 package cats.effect.kernel
 
 import cats.implicits._
+import cats.data.{OptionT}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -44,4 +45,50 @@ trait Async[F[_]] extends AsyncPlatform[F] with Sync[F] with Temporal[F, Throwab
 
 object Async {
   def apply[F[_]](implicit F: Async[F]): F.type = F
+
+  implicit def asyncForOptionT[F[_]](implicit F0: Async[F]): Async[OptionT[F, *]] =
+    new OptionTAsync[F] {
+      override implicit protected def F: Async[F] = F0
+    }
+
+  private[effect] trait OptionTAsync[F[_]]
+      extends Async[OptionT[F, *]]
+      with Sync.OptionTSync[F]
+      with Temporal.OptionTTemporal[F, Throwable] {
+
+    implicit protected def F: Async[F]
+
+    def async[A](k: (Either[Throwable, A] => Unit) => OptionT[F, Option[OptionT[F, Unit]]])
+        : OptionT[F, A] =
+      OptionT.liftF(F.async(k.andThen(_.value.map(_.flatten.map(_.value.void)))))
+
+    def evalOn[A](fa: OptionT[F, A], ec: ExecutionContext): OptionT[F, A] =
+      OptionT(F.evalOn(fa.value, ec))
+
+    def executionContext: OptionT[F, ExecutionContext] = OptionT.liftF(F.executionContext)
+
+    override def never[A]: OptionT[F, A] = OptionT.liftF(F.never)
+
+    // protected def delegate: Applicative[OptionT[F, *]] = OptionT.catsDataMonadForOptionT[F]
+
+    override def ap[A, B](
+        ff: OptionT[F, A => B]
+    )(fa: OptionT[F, A]): OptionT[F, B] = delegate.ap(ff)(fa)
+
+    override def pure[A](x: A): OptionT[F, A] = delegate.pure(x)
+
+    override def flatMap[A, B](fa: OptionT[F, A])(f: A => OptionT[F, B]): OptionT[F, B] =
+      delegate.flatMap(fa)(f)
+
+    override def tailRecM[A, B](a: A)(f: A => OptionT[F, Either[A, B]]): OptionT[F, B] =
+      delegate.tailRecM(a)(f)
+
+    override def raiseError[A](e: Throwable): OptionT[F, A] =
+      delegate.raiseError(e)
+
+    override def handleErrorWith[A](fa: OptionT[F, A])(
+        f: Throwable => OptionT[F, A]): OptionT[F, A] =
+      delegate.handleErrorWith(fa)(f)
+
+  }
 }
