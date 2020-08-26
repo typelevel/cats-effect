5ba3f9d044c320cd3546fa6c4335fdf09c2f4553
Raas Ahsan <raas.ahsan@gmail.com>
2020-08-26 00:29:45 -0500

    ls

--- build.sh
+++ build.sh
@@ -14,6 +14,11 @@ cp -R ./versions/2.x/site-docs/target/mdoc/ ./docs
 cp ./versions/2.x/site-docs/sidebars.json ./website/sidebars.json
 (cd website && yarn run version 2.x)
 
+ls ./docs/
+ls ./versions/2.x/site-docs/
+ls ./website/versioned_sidebars/
+ls ./website/versioned_docs/
+
 (cd website && yarn build)
 
 mv ./versions/2.x/core/jvm/target/scala-2.13/api ./website/build/cats-effect/
