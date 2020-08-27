a12d76edcaef280b8febd3206ed56ddc9e6f26c4
Raas Ahsan <raas.ahsan@gmail.com>
2020-08-26 23:24:20 -0500

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+rm -rf ./website/versions.json
+rm -rf ./website/versioned_sidebars
+rm -rf ./website/versioned_docs
+rm -rf ./website/sidebars.json
+rm -rf ./docs
+
 (cd website && yarn install)
 
 # Generate scaladoc and mdoc from each submodule
-(cd versions/2.x && sbt "; doc; docs/mdoc")
+(cd versions/2.x && sbt "; clean; doc; docs/clean; docs/mdoc")
 
 # Create Docusaurus directories
 mkdir -p ./website/versioned_sidebars
