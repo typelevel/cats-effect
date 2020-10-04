da1b289ee3febdcd7bc102d7a8e5fff1cff10788
Fabio Labella <fabio.labella2@gmail.com>
2020-10-04 12:59:11 +0100

    Inline definition of defaultCont

--- kernel/shared/src/main/scala/cats/effect/kernel/Async.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/Async.scala
@@ -18,6 +18,9 @@ package cats.effect.kernel
 
 import cats.syntax.all._
 
+import cats.arrow.FunctionK
+import java.util.concurrent.atomic.AtomicReference
+import scala.annotation.tailrec
 import scala.concurrent.{ExecutionContext, Future}
 
 trait Async[F[_]] extends AsyncPlatform[F] with Sync[F] with Temporal[F] {
@@ -68,6 +71,66 @@ trait Async[F[_]] extends AsyncPlatform[F] with Sync[F] with Temporal[F] {
 object Async {
   def apply[F[_]](implicit F: Async[F]): F.type = F
 
-  def defaultCont[F[_]: Async, A](body: Cont[F, A]): F[A] =
-    internal.DefaultCont.cont(body)
+  def defaultCont[F[_], A](body: Cont[F, A])(implicit F: Async[F]): F[A] = {
+    sealed trait State[A]
+    case class Initial[A]() extends State[A]
+    case class Value[A](v: Either[Throwable, A]) extends State[A]
+    case class Waiting[A](cb: Either[Throwable, A] => Unit) extends State[A]
+
+    F.delay(new AtomicReference[State[A]](Initial())).flatMap { state =>
+        def get: F[A] =
+    F.defer {
+      state.get match {
+        case Value(v) => F.fromEither(v)
+        case Initial() =>
+          F.async { cb =>
+            val waiting = Waiting(cb)
+
+            @tailrec
+            def loop(): Unit =
+              state.get match {
+                case s @ Initial() =>
+                  state.compareAndSet(s, waiting)
+                  loop()
+                case Waiting(_) => ()
+                case Value(v) => cb(v)
+              }
+
+            def onCancel = F.delay(state.compareAndSet(waiting, Initial())).void
+
+            F.delay(loop()).as(onCancel.some)
+          }
+        case Waiting(_) =>
+          /*
+           * - `cont` forbids concurrency, so no other `get` can be in Waiting.
+           * -  if a previous get has succeeded or failed and we are being sequenced
+           *    afterwards, it means `resume` has set the state to `Value`.
+           * - if a previous `get` has been interrupted and we are running as part of
+           *   its finalisers, the state would have been either restored to `Initial`
+           *   by the finaliser of that `get`, or set to `Value` by `resume`
+           */
+          sys.error("Impossible")
+      }
+    }
+
+      def resume(v: Either[Throwable, A]): Unit = {
+        @tailrec
+        def loop(): Unit =
+          state.get match {
+            case Value(_) => () /* idempotent, double calls are forbidden */
+            case s @ Initial() =>
+              if (!state.compareAndSet(s, Value(v))) loop()
+              else ()
+            case s @ Waiting(cb) =>
+              if (state.compareAndSet(s, Value(v))) cb(v)
+              else loop()
+          }
+
+        loop()
+      }
+
+
+      body[F].apply(resume, get, FunctionK.id)
+    }
+  }
 }
