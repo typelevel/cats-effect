b41eb3798d7af2d681918bf541c84865b230cbd2
Tim Spence <timothywspence@gmail.com>
2020-10-12 17:27:35 +0100

    Revert compiler flag changes

--- project/Common.scala
+++ project/Common.scala
@@ -36,6 +36,6 @@ object Common extends AutoPlugin {
         if (isDotty.value)
           Nil
         else
-          Seq("-Xcheckinit", "-Ypatmat-exhaust-depth", "off")
+          Seq("-Xcheckinit")
       })
 }
