067e84daa17669dbccf1118da02fc35a9b768f4f
Vasil Vasilev <vasilv@protonmail.com>
2020-10-25 20:55:40 +0100

    Tagless Console and other missing goodies

--- std/shared/src/main/scala/cats/effect/std/Console.scala
+++ std/shared/src/main/scala/cats/effect/std/Console.scala
@@ -21,7 +21,9 @@
 
 package cats.effect.std
 
-import cats.Show
+import cats.kernel.Monoid
+import cats.{~>, Applicative, Functor, Show}
+import cats.data.{EitherT, IorT, Kleisli, OptionT, ReaderWriterStateT, StateT, WriterT}
 import cats.effect.kernel.Sync
 import cats.syntax.show._
 
@@ -42,7 +44,9 @@ import java.nio.charset.{Charset, CodingErrorAction, MalformedInputException}
  *   import cats.effect.kernel.Sync
  *   import cats.syntax.all._
  *
- *   def myProgram[F[_]: Sync]: F[Unit] =
+ *   implicit val console = Console.sync[F]
+ *
+ *   def myProgram[F[_]: Console]: F[Unit] =
  *     for {
  *       _ <- Console[F].println("Please enter your name: ")
  *       n <- Console[F].readLine
@@ -51,7 +55,7 @@ import java.nio.charset.{Charset, CodingErrorAction, MalformedInputException}
  *     } yield ()
  * }}}
  */
-final class Console[F[_]] private (val F: Sync[F]) extends AnyVal {
+trait Console[F[_]] { self =>
 
   /**
    * Reads a line as a string from the standard input using the platform's
@@ -82,119 +86,74 @@ final class Console[F[_]] private (val F: Sync[F]) extends AnyVal {
    * @return an effect that describes reading the user's input from the standard
    *         input as a string
    */
-  def readLineWithCharset(charset: Charset): F[String] =
-    F.interruptible(false) {
-      val in = System.in
-      val decoder = charset
-        .newDecoder()
-        .onMalformedInput(CodingErrorAction.REPORT)
-        .onUnmappableCharacter(CodingErrorAction.REPLACE)
-      val bytes = ByteBuffer.allocate(64)
-      val builder = new JStringBuilder()
-
-      def decodeNext(): CharBuffer = {
-        bytes.clear()
-        decodeNextLoop()
-      }
-
-      @tailrec
-      def decodeNextLoop(): CharBuffer = {
-        val b = in.read()
-        if (b == -1) null
-        else {
-          bytes.put(b.toByte)
-          val limit = bytes.limit()
-          val position = bytes.position()
-          var result: CharBuffer = null
-          try {
-            bytes.flip()
-            result = decoder.decode(bytes)
-          } catch {
-            case _: MalformedInputException =>
-              bytes.limit(limit)
-              bytes.position(position)
-          }
-          if (result == null) decodeNextLoop() else result
-        }
-      }
-
-      @tailrec
-      def loop(): String = {
-        val buffer = decodeNext()
-        if (buffer == null) {
-          val result = builder.toString()
-          if (result.nonEmpty) result
-          else throw new EOFException()
-        } else {
-          val decoded = buffer.toString()
-          if (decoded == "\n") {
-            val len = builder.length()
-            if (len > 0) {
-              if (builder.charAt(len - 1) == '\r') {
-                builder.deleteCharAt(len - 1)
-              }
-            }
-            builder.toString()
-          } else {
-            builder.append(decoded)
-            loop()
-          }
-        }
-      }
-
-      loop()
-    }
+  def readLineWithCharset(charset: Charset): F[String]
 
   /**
    * Prints a value to the standard output using the implicit `cats.Show`
    * instance.
    *
    * @param a value to be printed to the standard output
+   * @param S implicit `cats.Show[A]` instance, defaults to `cats.Show.fromToString`
    */
-  def print[A: Show](a: A): F[Unit] = {
-    val text = a.show
-    F.blocking(System.out.print(text))
-  }
+  def print[A](a: A)(implicit S: Show[A] = Show.fromToString): F[Unit]
 
   /**
    * Prints a value to the standard output followed by a new line using the
    * implicit `cats.Show` instance.
    *
    * @param a value to be printed to the standard output
+   * @param S implicit `cats.Show[A]` instance, defaults to `cats.Show.fromToString`
    */
-  def println[A: Show](a: A): F[Unit] = {
-    val text = a.show
-    F.blocking(System.out.println(text))
-  }
+  def println[A](a: A)(implicit S: Show[A] = Show.fromToString): F[Unit]
 
   /**
    * Prints a value to the standard error output using the implicit `cats.Show`
    * instance.
    *
    * @param a value to be printed to the standard error output
+   * @param S implicit `cats.Show[A]` instance, defaults to `cats.Show.fromToString`
    */
-  def error[A: Show](a: A): F[Unit] = {
-    val text = a.show
-    F.blocking(System.err.print(text))
-  }
+  def error[A](a: A)(implicit S: Show[A] = Show.fromToString): F[Unit]
 
   /**
    * Prints a value to the standard error output followed by a new line using
    * the implicit `cast.Show` instance.
    *
    * @param a value to be printed to the standard error output
+   * @param S implicit `cats.Show[A]` instance, defaults to `cats.Show.fromToString`
    */
-  def errorln[A: Show](a: A): F[Unit] = {
-    val text = a.show
-    F.blocking(System.err.println(text))
-  }
+  def errorln[A](a: A)(implicit S: Show[A] = Show.fromToString): F[Unit]
+
+  /**
+   * Modifies the context in which this console operates using the natural
+   * transformation `f`.
+   *
+   * @return a console in the new context obtained by mapping the current one
+   *         using `f`
+   */
+  def mapK[G[_]](f: F ~> G): Console[G] =
+    new Console[G] {
+      def readLineWithCharset(charset: Charset): G[String] =
+        f(self.readLineWithCharset(charset))
+
+      def print[A](a: A)(implicit S: Show[A]): G[Unit] =
+        f(self.print(a))
+
+      def println[A](a: A)(implicit S: Show[A]): G[Unit] =
+        f(self.println(a))
+
+      def error[A](a: A)(implicit S: Show[A]): G[Unit] =
+        f(self.error(a))
+
+      def errorln[A](a: A)(implicit S: Show[A]): G[Unit] =
+        f(self.errorln(a))
+    }
 }
 
 object Console {
 
   /**
-   * Allows access to `Console` functionality for `F` data types that are
-   * [[Sync]].
+   * Summoner method for `Console` instances.
    *
    * For printing to the standard output:
    * {{{
@@ -213,6 +172,153 @@ object Console {
    *   Console[F].readLine
    * }}}
    */
-  def apply[F[_]](implicit F: Sync[F]): Console[F] =
-    new Console[F](F)
+  def apply[F[_]](implicit C: Console[F]): C.type = C
+
+  /**
+   * Constructs a `Console` instance for `F` data types that are [[Sync]].
+   */
+  def make[F[_]](implicit F: Sync[F]): Console[F] =
+    new SyncConsole[F]
+
+  /**
+   * [[Console]] instance built for `cats.data.EitherT` values initialized with
+   * any `F` data type that also implements `Console`.
+   */
+  implicit def catsEitherTConsole[F[_]: Console: Functor, L]: Console[EitherT[F, L, *]] =
+    Console[F].mapK(EitherT.liftK)
+
+  /**
+   * [[Console]] instance built for `cats.data.Kleisli` values initialized with
+   * any `F` data type that also implements `Console`.
+   */
+  implicit def catsKleisliConsole[F[_]: Console, R]: Console[Kleisli[F, R, *]] =
+    Console[F].mapK(Kleisli.liftK)
+
+  /**
+   * [[Console]] instance built for `cats.data.OptionT` values initialized with
+   * any `F` data type that also implements `Console`.
+   */
+  implicit def catsOptionTConsole[F[_]: Console: Functor]: Console[OptionT[F, *]] =
+    Console[F].mapK(OptionT.liftK)
+
+  /**
+   * [[Console]] instance built for `cats.data.StateT` values initialized with
+   * any `F` data type that also implements `Console`.
+   */
+  implicit def catsStateTConsole[F[_]: Console: Applicative, S]: Console[StateT[F, S, *]] =
+    Console[F].mapK(StateT.liftK)
+
+  /**
+   * [[Console]] instance built for `cats.data.WriterT` values initialized with
+   * any `F` data type that also implements `Console`.
+   */
+  implicit def catsWriterTConsole[
+      F[_]: Console: Applicative,
+      L: Monoid
+  ]: Console[WriterT[F, L, *]] =
+    Console[F].mapK(WriterT.liftK)
+
+  /**
+   * [[Console]] instance built for `cats.data.IorT` values initialized with any
+   * `F` data type that also implements `Console`.
+   */
+  implicit def catsIorTConsole[F[_]: Console: Functor, L]: Console[IorT[F, L, *]] =
+    Console[F].mapK(IorT.liftK)
+
+  /**
+   * [[Console]] instance built for `cats.data.ReaderWriterStateT` values
+   * initialized with any `F` data type that also implements `Console`.
+   */
+  implicit def catsReaderWriterStateTConsole[
+      F[_]: Console: Applicative,
+      E,
+      L: Monoid,
+      S
+  ]: Console[ReaderWriterStateT[F, E, L, S, *]] =
+    Console[F].mapK(ReaderWriterStateT.liftK)
+
+  private final class SyncConsole[F[_]](implicit F: Sync[F]) extends Console[F] {
+    def readLineWithCharset(charset: Charset): F[String] =
+      F.interruptible(false) {
+        val in = System.in
+        val decoder = charset
+          .newDecoder()
+          .onMalformedInput(CodingErrorAction.REPORT)
+          .onUnmappableCharacter(CodingErrorAction.REPLACE)
+        val bytes = ByteBuffer.allocate(64)
+        val builder = new JStringBuilder()
+
+        def decodeNext(): CharBuffer = {
+          bytes.clear()
+          decodeNextLoop()
+        }
+
+        @tailrec
+        def decodeNextLoop(): CharBuffer = {
+          val b = in.read()
+          if (b == -1) null
+          else {
+            bytes.put(b.toByte)
+            val limit = bytes.limit()
+            val position = bytes.position()
+            var result: CharBuffer = null
+            try {
+              bytes.flip()
+              result = decoder.decode(bytes)
+            } catch {
+              case _: MalformedInputException =>
+                bytes.limit(limit)
+                bytes.position(position)
+            }
+            if (result == null) decodeNextLoop() else result
+          }
+        }
+
+        @tailrec
+        def loop(): String = {
+          val buffer = decodeNext()
+          if (buffer == null) {
+            val result = builder.toString()
+            if (result.nonEmpty) result
+            else throw new EOFException()
+          } else {
+            val decoded = buffer.toString()
+            if (decoded == "\n") {
+              val len = builder.length()
+              if (len > 0) {
+                if (builder.charAt(len - 1) == '\r') {
+                  builder.deleteCharAt(len - 1)
+                }
+              }
+              builder.toString()
+            } else {
+              builder.append(decoded)
+              loop()
+            }
+          }
+        }
+
+        loop()
+      }
+
+    def print[A](a: A)(implicit S: Show[A] = Show.fromToString): F[Unit] = {
+      val text = a.show
+      F.blocking(System.out.print(text))
+    }
+
+    def println[A](a: A)(implicit S: Show[A] = Show.fromToString): F[Unit] = {
+      val text = a.show
+      F.blocking(System.out.println(text))
+    }
+
+    def error[A](a: A)(implicit S: Show[A] = Show.fromToString): F[Unit] = {
+      val text = a.show
+      F.blocking(System.err.print(text))
+    }
+
+    def errorln[A](a: A)(implicit S: Show[A] = Show.fromToString): F[Unit] = {
+      val text = a.show
+      F.blocking(System.err.println(text))
+    }
+  }
 }
