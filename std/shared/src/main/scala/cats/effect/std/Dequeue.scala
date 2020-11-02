5211e07126fa8f5cc9b649557bf91446a22aa904
Tim Spence <timothywspence@gmail.com>
2020-11-02 09:45:53 +0000

    Extract BankersQueue to internal

--- std/shared/src/main/scala/cats/effect/std/Dequeue.scala
+++ std/shared/src/main/scala/cats/effect/std/Dequeue.scala
@@ -19,6 +19,7 @@ package cats.effect.std
 import cats.~>
 import cats.effect.kernel.{Deferred, GenConcurrent, Ref}
 import cats.effect.kernel.syntax.all._
+import cats.effect.std.internal.BankersQueue
 import cats.syntax.all._
 
 import scala.collection.immutable.{Queue => ScalaQueue}
@@ -222,60 +223,4 @@ object Dequeue {
       State(BankersQueue.empty, 0, ScalaQueue.empty, ScalaQueue.empty)
   }
 
-  /**
-   * A banker's dequeue a la Okasaki
-   *
-   * Maintains the invariants that:
-   * - frontLen <= rebalanceConstant * backLen + 1
-   * - backLen <= rebalanceConstant * frontLen + 1
-   */
-  private[std] final case class BankersQueue[A](
-      front: List[A],
-      frontLen: Int,
-      back: List[A],
-      backLen: Int) {
-    import BankersQueue._
-
-    def nonEmpty: Boolean = frontLen > 0 || backLen > 0
-
-    def pushFront(a: A): BankersQueue[A] =
-      BankersQueue(a :: front, frontLen + 1, back, backLen).rebalance()
-
-    def pushBack(a: A): BankersQueue[A] =
-      BankersQueue(front, frontLen, a :: back, backLen + 1).rebalance()
-
-    def tryPopFront: (BankersQueue[A], Option[A]) =
-      if (frontLen > 0)
-        BankersQueue(front.tail, frontLen - 1, back, backLen).rebalance() -> Some(front.head)
-      else if (backLen > 0) BankersQueue(front, frontLen, Nil, 0) -> Some(back.head)
-      else this -> None
-
-    def tryPopBack: (BankersQueue[A], Option[A]) =
-      if (backLen > 0)
-        BankersQueue(front, frontLen, back.tail, backLen - 1).rebalance() -> Some(back.head)
-      else if (frontLen > 0) BankersQueue(Nil, 0, back, backLen) -> Some(front.head)
-      else this -> None
-
-    def rebalance(): BankersQueue[A] =
-      if (frontLen > rebalanceConstant * backLen + 1) {
-        val i = (frontLen + backLen) / 2
-        val j = frontLen + backLen - i
-        val f = front.take(i)
-        val b = back ++ f.drop(i).reverse
-        BankersQueue(f, i, b, j)
-      } else if (backLen > rebalanceConstant * frontLen + 1) {
-        val i = (frontLen + backLen) / 2
-        val j = frontLen + backLen - i
-        val f = front ++ back.drop(j).reverse
-        val b = back.take(j)
-        BankersQueue(f, i, b, j)
-      } else this
-
-  }
-
-  object BankersQueue {
-    val rebalanceConstant = 2
-
-    def empty[A]: BankersQueue[A] = BankersQueue(Nil, 0, Nil, 0)
-  }
 }
