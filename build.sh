c13a658b99acdd40f80e34d28c5d99cc10558f46
Daniel Spiewak <djspiewak@gmail.com>
2020-11-08 21:52:37 -0700

    Fixed version build order to make 2.x primary

--- build.sh
+++ build.sh
@@ -13,23 +13,23 @@ rm -rf ./docs
 (cd website && yarn install)
 
 # Generate scaladoc and mdoc from each submodule
-(cd versions/2.x && sbt clean doc docs/mdoc)
-(cd versions/3.x && sbt clean coreJVM/doc docs/mdoc)
+(cd versions/2.x && sbt doc docs/mdoc)
+(cd versions/3.x && sbt coreJVM/doc docs/mdoc)
 
 # Create Docusaurus directories
 mkdir -p ./website/versioned_sidebars
 mkdir -p ./website/versioned_docs
 
-mv ./versions/2.x/site-docs/target/mdoc ./docs
-cp ./versions/2.x/site-docs/sidebars.json ./website/sidebars.json
-(cd website && yarn run version 2.x)
-
-
-rm -rf ./website/sidebars.json
-rm -rf ./docs
 mv ./versions/3.x/site-docs/target/mdoc ./docs
 cp ./versions/3.x/site-docs/sidebars.json ./website/sidebars.json
 (cd website && yarn run version 3.x)
+
+rm -rf ./website/sidebars.json
+rm -rf ./docs
+
+mv ./versions/2.x/site-docs/target/mdoc ./docs
+cp ./versions/2.x/site-docs/sidebars.json ./website/sidebars.json
+(cd website && yarn run version 2.x)
 
 if [[ "$cmd" == "host" ]]; then
   (cd website && yarn start)
