4839e5d59eb60b71939ea7d5fc6eb098ac94683b
Fabio Labella <fabio.labella2@gmail.com>
2020-11-27 23:48:49 +0000

    Restate MiniSemaphore tests in terms of withPermit

--- core/shared/src/test/scala/cats/effect/kernel/MiniSemaphoreSpec.scala
+++ core/shared/src/test/scala/cats/effect/kernel/MiniSemaphoreSpec.scala
@@ -19,79 +19,76 @@ package effect
 package kernel
 
 import scala.concurrent.duration._
-import java.util.concurrent.TimeoutException
 
 class MiniSemaphoreSpec extends BaseSpec { outer =>
 
   "mini semaphore" should {
-
     "throw on negative n" in real {
-      IO.defer(MiniSemaphore[IO](-1)).attempt.flatMap { res =>
-        IO {
-          res must beLike {
-            case Left(e) => e must haveClass[IllegalArgumentException]
-          }
-        }
+      IO.defer(MiniSemaphore[IO](-1)).mustFailWith[IllegalArgumentException]
+    }
 
+    "block if no permits available" in ticked { implicit ticker =>
+      MiniSemaphore[IO](0).flatMap { sem =>
+        sem.withPermit(IO.unit)
+      } must nonTerminate
+    }
+
+    "execute action if permit is available for it" in real {
+      MiniSemaphore[IO](1).flatMap { sem =>
+        sem.withPermit(IO.unit).mustEqual(())
       }
     }
 
-    "block if no permits available" in real {
-      for {
-        sem <- MiniSemaphore[IO](0)
-        r <- sem.acquire.timeout(10.millis).attempt
-        res <- IO {
-          r must beLike {
-            case Left(e) => e must haveClass[TimeoutException]
-          }
-        }
-      } yield res
-    }
+    "unblock when permit is released" in ticked { implicit ticker =>
+      val p =
+        for {
+          sem <- MiniSemaphore[IO](1)
+          ref <- IO.ref(0)
+          _ <- sem.withPermit { IO.sleep(1.second) >> ref.set(1) }.start
+          _ <- IO.sleep(500.millis)
+          _ <- sem.withPermit(IO.unit)
+          v <- ref.get
+        } yield v
 
-    "acquire if permit is available" in real {
-      for {
-        sem <- MiniSemaphore[IO](1)
-        r <- sem.acquire.timeout(10.millis).attempt
-        res <- IO { r must beEqualTo(Right(())) }
-      } yield res
-
-    }
-
-    "unblock when permit is released" in real {
-      for {
-        sem <- MiniSemaphore[IO](1)
-        _ <- sem.acquire
-        f <- sem.acquire.start
-        _ <- IO.sleep(100.millis)
-        _ <- sem.release
-        r <- f.joinAndEmbedNever
-        res <- IO { r must beEqualTo(()) }
-      } yield res
+      p must completeAs(1)
     }
 
     "release permit if withPermit errors" in real {
       for {
         sem <- MiniSemaphore[IO](1)
-        _ <- sem.withPermit(IO.raiseError(new RuntimeException("BOOM"))).attempt
-        //withPermit should have released the permit again
-        r <- sem.acquire.timeout(10.millis).attempt
-        res <- IO { r must beEqualTo(Right(())) }
+        _ <- sem.withPermit(IO.raiseError(new Exception)).attempt
+        res <- sem.withPermit(IO.unit).mustEqual(())
       } yield res
     }
 
-    "acquire is cancelable" in real {
-      for {
-        sem <- MiniSemaphore[IO](1)
-        _ <- sem.acquire
-        f <- sem.acquire.start
-        _ <- IO.sleep(100.millis)
-        _ <- f.cancel
-        _ <- sem.release
-        //The fiber that was cancelled should not have acquired the permit
-        r <- sem.acquire.timeout(10.millis).attempt
-        res <- IO { r must beEqualTo(Right(())) }
-      } yield res
-    }
+    // // TODO falsify this test
+    // "release permit if action gets canceled" in ticked { implicit ticker =>
+    //   val p =
+    //     for {
+    //       sem <- MiniSemaphore[IO](1)
+    //       fiber <- sem.withPermit(IO.never).start
+    //       _ <- IO.sleep(1.second)
+    //       _ <- fiber.cancel
+    //       _ <- sem.withPermit(IO.unit)
+    //     } yield ()
+
+    //   p must completeAs(())
+    // }
+
+    // "allow cancelation if blocked waiting for permit" in ticked { implicit ticker =>
+    //   for {
+    //     sem <- MiniSemaphore[IO](1)
+    //     _ <- sem.acquire
+    //     f <- sem.acquire.start
+    //     _ <- IO.sleep(100.millis)
+    //     _ <- f.cancel
+    //     _ <- sem.release
+    //     //The fiber that was cancelled should not have acquired the permit
+    //     r <- sem.acquire.timeout(10.millis).attempt
+    //     res <- IO { r must beEqualTo(Right(())) }
+    //   } yield res
+
+   // }
   }
 
 }
