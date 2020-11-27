580bcded2599d09c7dd94cb75dccb3838b21337f
Fabio Labella <fabio.labella2@gmail.com>
2020-11-27 18:54:17 +0000

    Add sketch for MiniSemaphore with uncancelable

--- kernel/shared/src/main/scala/cats/effect/kernel/MiniSemaphore.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/MiniSemaphore.scala
@@ -19,7 +19,7 @@ package effect
 package kernel
 
 import cats.syntax.all._
-
+import cats.effect.kernel.syntax.all._
 import scala.collection.immutable.{Queue => ScalaQueue}
 
 /**
@@ -46,6 +46,59 @@ private[kernel] abstract class MiniSemaphore[F[_]] {
 }
 
 private[kernel] object MiniSemaphore {
+
+  def mini[F[_]](n: Int)(implicit F: GenConcurrent[F, _]): F[MiniSemaphore[F]] = {
+
+    // this representation appears more intuitive at first, but it's
+    // actually weird, since a semaphore at zero permits is Right(0)
+    // in `acquire`, but Left(empty) in `release`
+    type State = Either[ScalaQueue[Deferred[F, Unit]], Int]
+
+    F.ref[State](Right(n)).map { state =>
+      new MiniSemaphore[F] {
+        def acquire: F[Unit] =
+          F.uncancelable { poll =>
+            F.deferred[Unit].flatMap { wait =>
+              val cleanup = state.update {
+                case Left(waiting) => Left(waiting.filterNot(_ eq wait))
+                case Right(m) => Right(m)
+              }
+
+              state.modify {
+                case Right(permits) =>
+                  if (permits == 0)
+                    Left(ScalaQueue(wait)) -> poll(wait.get).onCancel(cleanup)
+                  else
+                    Right(permits - 1) -> ().pure[F]
+
+                case Left(waiting) =>
+                  Left(waiting :+ wait) -> poll(wait.get).onCancel(cleanup)
+              }.flatten
+            }
+          }
+
+        def release: F[Unit] =
+          state.modify { st =>
+            st match {
+              case Left(waiting) =>
+                if (waiting.isEmpty)
+                  Right(1) -> ().pure[F]
+                else
+                  Left(waiting.tail) -> waiting.head.complete(()).void
+              case Right(m) =>
+                Right(m + 1) -> ().pure[F]
+            }
+          }.flatten
+            .uncancelable
+
+        def withPermit[A](fa: F[A]): F[A] =
+          F.uncancelable { poll =>
+            poll(acquire) >> poll(fa).guarantee(release)
+          }
+      }
+    }
+  }
+
 
   /**
    * Creates a new `Semaphore`, initialized with `n` available permits.
