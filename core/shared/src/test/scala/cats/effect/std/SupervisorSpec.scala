49e803ef669b5bfb052e9b0985600d24eac8d8d5
Raas Ahsan <raas.ahsan@gmail.com>
2020-12-09 00:28:28 -0600

    Supervisor spec

---
+++ core/shared/src/test/scala/cats/effect/std/SupervisorSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect
+package std
+
+import cats.effect.kernel.{Deferred, Outcome}
+
+class SupervisorSpec extends BaseSpec {
+
+  "Supervisor" should {
+    "start a fiber" in ticked { implicit ticker =>
+      val test = Supervisor[IO].use { supervisor => supervisor.supervise(IO(1)).flatten }
+
+      test must completeAs(Outcome.succeeded[IO, Throwable, Int](IO.pure(1)))
+    }
+
+    "cancel active fibers when supervisor exits" in ticked { implicit ticker =>
+      val test = for {
+        joinDef <- Deferred[IO, IO[Outcome[IO, Throwable, Unit]]]
+        _ <- Supervisor[IO].use { supervisor =>
+          supervisor.supervise(IO.never[Unit]).flatMap(joinDef.complete(_))
+        }
+        outcome <- joinDef.get.flatten
+      } yield outcome
+
+      test must completeAs(Outcome.canceled[IO, Throwable, Unit])
+    }
+  }
+
+}
