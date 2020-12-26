01204ea6b61a75b6d26f2efc9a71d6beea05f52f
James Yoo <jyoo980@gmail.com>
2020-12-26 12:18:14 -0800

    refactoring tests in PQueueSpec and QueueSpec

--- core/shared/src/test/scala/cats/effect/std/QueueSpec.scala
+++ core/shared/src/test/scala/cats/effect/std/QueueSpec.scala
@@ -38,58 +38,62 @@ class BoundedQueueSpec extends BaseSpec with QueueTests[Queue] {
   private def boundedQueueTests(
       name: String,
       constructor: Int => IO[Queue[IO, Int]]): Fragments = {
-    s"$name - demonstrate offer and take with zero capacity" in real {
-      for {
-        q <- constructor(0)
-        _ <- q.offer(1).start
-        v1 <- q.take
-        f <- q.take.start
-        _ <- q.offer(2)
-        v2 <- f.joinWithNever
-        r <- IO((v1 must beEqualTo(1)) and (v2 must beEqualTo(2)))
-      } yield r
-    }
 
-    s"$name - async take with zero capacity" in realWithRuntime { implicit rt =>
-      for {
-        q <- constructor(0)
-        _ <- q.offer(1).start
-        v1 <- q.take
-        _ <- IO(v1 must beEqualTo(1))
-        ff <- IO(q.take.unsafeToFuture()).start
-        f <- ff.joinWithNever
-        _ <- IO(f.value must beEqualTo(None))
-        _ <- q.offer(2)
-        v2 <- IO.fromFuture(IO.pure(f))
-        r <- IO(v2 must beEqualTo(2))
-      } yield r
-    }
+    name >> {
 
-    s"$name - offer/take with zero capacity" in real {
-      val count = 1000
+      "demonstrate offer and take with zero capacity" in real {
+        for {
+          q <- constructor(0)
+          _ <- q.offer(1).start
+          v1 <- q.take
+          f <- q.take.start
+          _ <- q.offer(2)
+          v2 <- f.joinWithNever
+          r <- IO((v1 must beEqualTo(1)) and (v2 must beEqualTo(2)))
+        } yield r
+      }
 
-      def producer(q: Queue[IO, Int], n: Int): IO[Unit] =
-        if (n > 0) q.offer(count - n).flatMap(_ => producer(q, n - 1))
-        else IO.unit
+      "async take with zero capacity" in realWithRuntime { implicit rt =>
+        for {
+          q <- constructor(0)
+          _ <- q.offer(1).start
+          v1 <- q.take
+          _ <- IO(v1 must beEqualTo(1))
+          ff <- IO(q.take.unsafeToFuture()).start
+          f <- ff.joinWithNever
+          _ <- IO(f.value must beEqualTo(None))
+          _ <- q.offer(2)
+          v2 <- IO.fromFuture(IO.pure(f))
+          r <- IO(v2 must beEqualTo(2))
+        } yield r
+      }
 
-      def consumer(
-          q: Queue[IO, Int],
-          n: Int,
-          acc: ScalaQueue[Int] = ScalaQueue.empty
-      ): IO[Long] =
-        if (n > 0)
-          q.take.flatMap { a => consumer(q, n - 1, acc.enqueue(a)) }
-        else
-          IO.pure(acc.foldLeft(0L)(_ + _))
+      "offer/take with zero capacity" in real {
+        val count = 1000
 
-      for {
-        q <- constructor(0)
-        p <- producer(q, count).start
-        c <- consumer(q, count).start
-        _ <- p.join
-        v <- c.joinWithNever
-        r <- IO(v must beEqualTo(count.toLong * (count - 1) / 2))
-      } yield r
+        def producer(q: Queue[IO, Int], n: Int): IO[Unit] =
+          if (n > 0) q.offer(count - n).flatMap(_ => producer(q, n - 1))
+          else IO.unit
+
+        def consumer(
+            q: Queue[IO, Int],
+            n: Int,
+            acc: ScalaQueue[Int] = ScalaQueue.empty
+        ): IO[Long] =
+          if (n > 0)
+            q.take.flatMap { a => consumer(q, n - 1, acc.enqueue(a)) }
+          else
+            IO.pure(acc.foldLeft(0L)(_ + _))
+
+        for {
+          q <- constructor(0)
+          p <- producer(q, count).start
+          c <- consumer(q, count).start
+          _ <- p.join
+          v <- c.joinWithNever
+          r <- IO(v must beEqualTo(count.toLong * (count - 1) / 2))
+        } yield r
+      }
     }
 
     negativeCapacityConstructionTests(name, constructor)
@@ -167,12 +171,16 @@ trait QueueTests[Q[_[_], _]] { self: BaseSpec =>
       name: String,
       constructor: Int => IO[Q[IO, Int]]
   ): Fragments = {
-    s"$name - should raise an exception when constructed with zero capacity" in real {
-      val test = IO.defer(constructor(0)).attempt
-      test.flatMap { res =>
-        IO {
-          res must beLike {
-            case Left(e) => e must haveClass[IllegalArgumentException]
+
+    name >> {
+
+      "should raise an exception when constructed with zero capacity" in real {
+        val test = IO.defer(constructor(0)).attempt
+        test.flatMap { res =>
+          IO {
+            res must beLike {
+              case Left(e) => e must haveClass[IllegalArgumentException]
+            }
           }
         }
       }
@@ -183,12 +191,16 @@ trait QueueTests[Q[_[_], _]] { self: BaseSpec =>
       name: String,
       constructor: Int => IO[Q[IO, Int]]
   ): Fragments = {
-    s"$name - should raise an exception when constructed with a negative capacity" in real {
-      val test = IO.defer(constructor(-1)).attempt
-      test.flatMap { res =>
-        IO {
-          res must beLike {
-            case Left(e) => e must haveClass[IllegalArgumentException]
+
+    name >> {
+
+      "should raise an exception when constructed with a negative capacity" in real {
+        val test = IO.defer(constructor(-1)).attempt
+        test.flatMap { res =>
+          IO {
+            res must beLike {
+              case Left(e) => e must haveClass[IllegalArgumentException]
+            }
           }
         }
       }
@@ -201,13 +213,17 @@ trait QueueTests[Q[_[_], _]] { self: BaseSpec =>
       offer: (Q[IO, Int], Int) => IO[Unit],
       tryOffer: (Q[IO, Int], Int) => IO[Boolean],
       expected: Boolean): Fragments = {
-    s"$name - should return false on tryOffer when the queue is full" in real {
-      for {
-        q <- constructor(1)
-        _ <- offer(q, 0)
-        v <- tryOffer(q, 1)
-        r <- IO(v must beEqualTo(expected))
-      } yield r
+
+    name >> {
+
+      "should return false on tryOffer when the queue is full" in real {
+        for {
+          q <- constructor(1)
+          _ <- offer(q, 0)
+          v <- tryOffer(q, 1)
+          r <- IO(v must beEqualTo(expected))
+        } yield r
+      }
     }
   }
 
@@ -217,31 +233,35 @@ trait QueueTests[Q[_[_], _]] { self: BaseSpec =>
       offer: (Q[IO, Int], Int) => IO[Unit],
       take: Q[IO, Int] => IO[Int]
   ): Fragments = {
-    s"$name - offer/take over capacity" in real {
-      val count = 1000
 
-      def producer(q: Q[IO, Int], n: Int): IO[Unit] =
-        if (n > 0) offer(q, count - n).flatMap(_ => producer(q, n - 1))
-        else IO.unit
+    name >> {
 
-      def consumer(
-          q: Q[IO, Int],
-          n: Int,
-          acc: ScalaQueue[Int] = ScalaQueue.empty
-      ): IO[Long] =
-        if (n > 0)
-          take(q).flatMap { a => consumer(q, n - 1, acc.enqueue(a)) }
-        else
-          IO.pure(acc.foldLeft(0L)(_ + _))
+      "offer/take over capacity" in real {
+        val count = 1000
 
-      for {
-        q <- constructor(10)
-        p <- producer(q, count).start
-        c <- consumer(q, count).start
-        _ <- p.join
-        v <- c.joinWithNever
-        r <- IO(v must beEqualTo(count.toLong * (count - 1) / 2))
-      } yield r
+        def producer(q: Q[IO, Int], n: Int): IO[Unit] =
+          if (n > 0) offer(q, count - n).flatMap(_ => producer(q, n - 1))
+          else IO.unit
+
+        def consumer(
+            q: Q[IO, Int],
+            n: Int,
+            acc: ScalaQueue[Int] = ScalaQueue.empty
+        ): IO[Long] =
+          if (n > 0)
+            take(q).flatMap { a => consumer(q, n - 1, acc.enqueue(a)) }
+          else
+            IO.pure(acc.foldLeft(0L)(_ + _))
+
+        for {
+          q <- constructor(10)
+          p <- producer(q, count).start
+          c <- consumer(q, count).start
+          _ <- p.join
+          v <- c.joinWithNever
+          r <- IO(v must beEqualTo(count.toLong * (count - 1) / 2))
+        } yield r
+      }
     }
   }
 
@@ -251,17 +271,21 @@ trait QueueTests[Q[_[_], _]] { self: BaseSpec =>
       offer: (Q[IO, Int], Int) => IO[Unit],
       take: Q[IO, Int] => IO[Int],
       tryTake: Q[IO, Int] => IO[Option[Int]]): Fragments = {
-    s"$name - demonstrate cancelable offer" in real {
-      for {
-        q <- constructor(1)
-        _ <- offer(q, 1)
-        f <- offer(q, 2).start
-        _ <- IO.sleep(10.millis)
-        _ <- f.cancel
-        v1 <- take(q)
-        v2 <- tryTake(q)
-        r <- IO((v1 must beEqualTo(1)) and (v2 must beEqualTo(None)))
-      } yield r
+
+    name >> {
+
+      "demonstrate cancelable offer" in real {
+        for {
+          q <- constructor(1)
+          _ <- offer(q, 1)
+          f <- offer(q, 2).start
+          _ <- IO.sleep(10.millis)
+          _ <- f.cancel
+          v1 <- take(q)
+          v2 <- tryTake(q)
+          r <- IO((v1 must beEqualTo(1)) and (v2 must beEqualTo(None)))
+        } yield r
+      }
     }
   }
 
@@ -270,39 +294,43 @@ trait QueueTests[Q[_[_], _]] { self: BaseSpec =>
       constructor: Int => IO[Q[IO, Int]],
       tryOffer: (Q[IO, Int], Int) => IO[Boolean],
       tryTake: Q[IO, Int] => IO[Option[Int]]): Fragments = {
-    s"$name - tryOffer/tryTake" in real {
-      val count = 1000
 
-      def producer(q: Q[IO, Int], n: Int): IO[Unit] =
-        if (n > 0) tryOffer(q, count - n).flatMap {
-          case true =>
-            producer(q, n - 1)
-          case false =>
-            IO.cede *> producer(q, n)
-        }
-        else IO.unit
+    name >> {
 
-      def consumer(
-          q: Q[IO, Int],
-          n: Int,
-          acc: ScalaQueue[Int] = ScalaQueue.empty
-      ): IO[Long] =
-        if (n > 0)
-          tryTake(q).flatMap {
-            case Some(a) => consumer(q, n - 1, acc.enqueue(a))
-            case None => IO.cede *> consumer(q, n, acc)
+      "tryOffer/tryTake" in real {
+        val count = 1000
+
+        def producer(q: Q[IO, Int], n: Int): IO[Unit] =
+          if (n > 0) tryOffer(q, count - n).flatMap {
+            case true =>
+              producer(q, n - 1)
+            case false =>
+              IO.cede *> producer(q, n)
           }
-        else
-          IO.pure(acc.foldLeft(0L)(_ + _))
+          else IO.unit
 
-      for {
-        q <- constructor(10)
-        p <- producer(q, count).start
-        c <- consumer(q, count).start
-        _ <- p.join
-        v <- c.joinWithNever
-        r <- IO(v must beEqualTo(count.toLong * (count - 1) / 2))
-      } yield r
+        def consumer(
+            q: Q[IO, Int],
+            n: Int,
+            acc: ScalaQueue[Int] = ScalaQueue.empty
+        ): IO[Long] =
+          if (n > 0)
+            tryTake(q).flatMap {
+              case Some(a) => consumer(q, n - 1, acc.enqueue(a))
+              case None => IO.cede *> consumer(q, n, acc)
+            }
+          else
+            IO.pure(acc.foldLeft(0L)(_ + _))
+
+        for {
+          q <- constructor(10)
+          p <- producer(q, count).start
+          c <- consumer(q, count).start
+          _ <- p.join
+          v <- c.joinWithNever
+          r <- IO(v must beEqualTo(count.toLong * (count - 1) / 2))
+        } yield r
+      }
     }
   }
 
@@ -313,48 +341,52 @@ trait QueueTests[Q[_[_], _]] { self: BaseSpec =>
       tryOffer: (Q[IO, Int], Int) => IO[Boolean],
       take: Q[IO, Int] => IO[Int],
       tryTake: Q[IO, Int] => IO[Option[Int]]): Fragments = {
-    s"$name - should return None on tryTake when the queue is empty" in real {
-      for {
-        q <- constructor(1)
-        v <- tryTake(q)
-        r <- IO(v must beNone)
-      } yield r
-    }
 
-    s"$name - demonstrate sequential offer and take" in real {
-      for {
-        q <- constructor(1)
-        _ <- offer(q, 1)
-        v1 <- take(q)
-        _ <- offer(q, 2)
-        v2 <- take(q)
-        r <- IO((v1 must beEqualTo(1)) and (v2 must beEqualTo(2)))
-      } yield r
-    }
+    name >> {
 
-    s"$name - demonstrate cancelable take" in real {
-      for {
-        q <- constructor(1)
-        f <- take(q).start
-        _ <- IO.sleep(10.millis)
-        _ <- f.cancel
-        v <- tryOffer(q, 1)
-        r <- IO(v must beTrue)
-      } yield r
-    }
+      "should return None on tryTake when the queue is empty" in real {
+        for {
+          q <- constructor(1)
+          v <- tryTake(q)
+          r <- IO(v must beNone)
+        } yield r
+      }
 
-    s"$name - async take" in realWithRuntime { implicit rt =>
-      for {
-        q <- constructor(10)
-        _ <- offer(q, 1)
-        v1 <- take(q)
-        _ <- IO(v1 must beEqualTo(1))
-        f <- IO(take(q).unsafeToFuture())
-        _ <- IO(f.value must beEqualTo(None))
-        _ <- offer(q, 2)
-        v2 <- IO.fromFuture(IO.pure(f))
-        r <- IO(v2 must beEqualTo(2))
-      } yield r
+      "demonstrate sequential offer and take" in real {
+        for {
+          q <- constructor(1)
+          _ <- offer(q, 1)
+          v1 <- take(q)
+          _ <- offer(q, 2)
+          v2 <- take(q)
+          r <- IO((v1 must beEqualTo(1)) and (v2 must beEqualTo(2)))
+        } yield r
+      }
+
+      "demonstrate cancelable take" in real {
+        for {
+          q <- constructor(1)
+          f <- take(q).start
+          _ <- IO.sleep(10.millis)
+          _ <- f.cancel
+          v <- tryOffer(q, 1)
+          r <- IO(v must beTrue)
+        } yield r
+      }
+
+      "async take" in realWithRuntime { implicit rt =>
+        for {
+          q <- constructor(10)
+          _ <- offer(q, 1)
+          v1 <- take(q)
+          _ <- IO(v1 must beEqualTo(1))
+          f <- IO(take(q).unsafeToFuture())
+          _ <- IO(f.value must beEqualTo(None))
+          _ <- offer(q, 2)
+          v2 <- IO.fromFuture(IO.pure(f))
+          r <- IO(v2 must beEqualTo(2))
+        } yield r
+      }
     }
   }
 }
