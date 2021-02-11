798d10c495d4ef0c7d3f6da39e18345d002458d5
Frank S. Thomas <frank@timepit.eu>
2021-02-11 20:00:43 +0100

    More test cases

--- scalafix/v3_0_0/input/src/main/scala/fix/PkgRewrites.scala
+++ scalafix/v3_0_0/input/src/main/scala/fix/PkgRewrites.scala
@@ -3,15 +3,19 @@ rule = "scala:fix.v3_0_0"
  */
 package fix
 
+import cats.effect.IO
 import cats.effect.concurrent.{ Deferred, Ref, Semaphore }
 
 object PkgRewrites {
   locally(Deferred)
   locally(cats.effect.concurrent.Deferred)
+  def f1(d: cats.effect.concurrent.Deferred[IO, Unit]): Deferred[IO, Unit] = d
 
   locally(Ref)
   locally(cats.effect.concurrent.Ref)
+  def f2(r: cats.effect.concurrent.Ref[IO, Unit]): Ref[IO, Unit] = r
 
   locally(Semaphore)
   locally(cats.effect.concurrent.Semaphore)
+  def f3(s: cats.effect.concurrent.Semaphore[IO]): Semaphore[IO] = s
 }
--- scalafix/v3_0_0/output/src/main/scala/fix/PkgRewrites.scala
+++ scalafix/v3_0_0/output/src/main/scala/fix/PkgRewrites.scala
@@ -1,15 +1,19 @@
 package fix
 
+import cats.effect.IO
 import cats.effect.{ Deferred, Ref }
 import cats.effect.std.Semaphore
 
 object PkgRewrites {
   locally(Deferred)
   locally(_root_.cats.effect.Deferred)
+  def f1(d: _root_.cats.effect.Deferred[IO, Unit]): Deferred[IO, Unit] = d
 
   locally(Ref)
   locally(_root_.cats.effect.Ref)
+  def f2(r: _root_.cats.effect.Ref[IO, Unit]): Ref[IO, Unit] = r
 
   locally(Semaphore)
   locally(cats.effect.std.Semaphore)
+  def f3(s: cats.effect.std.Semaphore[IO]): Semaphore[IO] = s
 }
