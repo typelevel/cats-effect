f83ca6287c13683024f769a5f275b47efe4ad773
Vasil Vasilev <vasil@vasilev.io>
2021-05-21 14:22:54 +0200

    Fix import ordering

--- scalafix/v2_4_0/tests/src/test/scala/fix/CatsEffectTests.scala
+++ scalafix/v2_4_0/tests/src/test/scala/fix/CatsEffectTests.scala
@@ -1,7 +1,7 @@
 package fix
 
-import scalafix.testkit.AbstractSemanticRuleSuite
 import org.scalatest.FunSuiteLike
+import scalafix.testkit.AbstractSemanticRuleSuite
 
 class CatsEffectTests extends AbstractSemanticRuleSuite with FunSuiteLike {
   runAllTests()
