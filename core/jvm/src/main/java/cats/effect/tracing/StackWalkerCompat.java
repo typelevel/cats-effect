7c827db4323f4da92ecc2662e14468df7e4b0af6
Vasil Vasilev <vasil@vasilev.io>
2021-05-29 16:29:11 +0200

    Add a StackWalker compatibility class with a similar API

---
+++ core/jvm/src/main/java/cats/effect/tracing/StackWalkerCompat.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020-2021 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect.tracing;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+class StackWalkerCompat {
+
+  private static final StackWalkerCompat INSTANCE = new StackWalkerCompat();
+
+  <T> T walk(Function<? super Stream<StackTraceElement>, T> f) {
+    return f.apply(Arrays.stream(new Throwable().getStackTrace()));
+  }
+
+  static StackWalkerCompat getInstance() {
+    return INSTANCE;
+  }
+}
