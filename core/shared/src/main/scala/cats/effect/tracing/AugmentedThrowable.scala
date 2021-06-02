5c2067e9803e752efb4dd2aa6155e956721cb6cb
Vasil Vasilev <vasil@vasilev.io>
2021-06-02 12:11:37 +0200

    Add the throwable augmentation logic

--- core/shared/src/main/scala/cats/effect/tracing/AugmentedThrowable.scala
+++ core/shared/src/main/scala/cats/effect/tracing/AugmentedThrowable.scala
@@ -16,4 +16,55 @@
 
 package cats.effect.tracing
 
-private[effect] final class AugmentedThrowable extends Throwable
+import scala.collection.mutable.ArrayBuffer
+
+private[effect] object AugmentedThrowable {
+
+  private[this] val runLoopFilter: Array[String] = Array("cats.effect.", "scala.runtime.")
+
+  def augmentThrowable(t: Throwable, events: RingBuffer): Unit = {
+    def filter(ste: StackTraceElement): Boolean = {
+      val name = ste.getClassName
+      var i = 0
+      val len = runLoopFilter.length
+      while (i < len) {
+        if (name.startsWith(runLoopFilter(i))) {
+          return true
+        }
+        i += 1
+      }
+
+      false
+    }
+
+    def dropRunLoopFrames(frames: Array[StackTraceElement]): Array[StackTraceElement] = {
+      val buffer = ArrayBuffer.empty[StackTraceElement]
+      var i = 0
+      val len = frames.length
+      while (i < len) {
+        val frame = frames(i)
+        if (!filter(frame)) {
+          buffer += frame
+        } else {
+          return buffer.toArray
+        }
+        i += 1
+      }
+
+      buffer.toArray
+    }
+
+    val stackTrace = t.getStackTrace
+    if (!stackTrace.isEmpty) {
+      val augmented = stackTrace.last.getClassName.indexOf('@') != -1
+      if (!augmented) {
+        val prefix = dropRunLoopFrames(stackTrace)
+        val suffix = events
+          .toList
+          .collect { case ev: TracingEvent.CallSite if ev.callSite ne null => ev.callSite }
+          .toArray
+        t.setStackTrace(prefix ++ suffix)
+      }
+    }
+  }
+}
