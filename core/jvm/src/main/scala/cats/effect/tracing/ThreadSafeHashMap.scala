a97c194d2a116f47befa9b677a1e28cc6d22032f
Vasil Vasilev <vasil@vasilev.io>
2021-06-05 13:34:10 +0200

    Initial code dump of ThreadSafeHashMap

---
+++ core/jvm/src/main/scala/cats/effect/tracing/ThreadSafeHashMap.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2020-2021 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect.tracing
+
+private final class ThreadSafeHashMap(initialCapacity: Int) {
+  private[this] var keysTable: Array[Class[_]] = new Array(initialCapacity)
+  private[this] var valsTable: Array[TracingEvent] = new Array(initialCapacity)
+  private[this] var size: Int = 0
+  private[this] var mask: Int = initialCapacity - 1
+  private[this] var capacity: Int = initialCapacity
+
+  def put(cls: Class[_], event: TracingEvent, hash: Int): Unit = this.synchronized {
+    val cap = capacity
+    if ((size << 4) / 3 >= cap) {
+      val newCap = cap << 1
+      val newKeysTable = new Array[Class[_]](newCap)
+      val newValsTable = new Array[TracingEvent](newCap)
+
+      val kt = keysTable
+      val vt = valsTable
+      var i = 0
+      while (i < cap) {
+        val c = kt(i)
+        if (c ne null) {
+          insert(newKeysTable, newValsTable, c, vt(i), System.identityHashCode(c))
+        }
+        i += 1
+      }
+
+      keysTable = newKeysTable
+      valsTable = newValsTable
+      mask = newCap - 1
+      capacity = newCap
+    }
+
+    insert(keysTable, valsTable, cls, event, hash)
+  }
+
+  /**
+   * ''Must'' be called with the lock on the whole `ThreadSafeHashMap` object
+   * already held. The `table` should contain at least one empty space to
+   * place the callback in.
+   */
+  private[this] def insert(
+      keysTable: Array[Class[_]],
+      valsTable: Array[TracingEvent],
+      cls: Class[_],
+      event: TracingEvent,
+      hash: Int
+  ): Unit = {
+    var idx = hash & mask
+    while (true) {
+      if (keysTable(idx) eq null) {
+        keysTable(idx) = cls
+        valsTable(idx) = event
+        size += 1
+        return
+      } else {
+        idx += 1
+        idx &= mask
+      }
+    }
+  }
+
+  def remove(cls: Class[_], hash: Int): Unit = this.synchronized {
+    val init = hash & mask
+    var idx = init
+
+    while (true) {
+      if (cls eq keysTable(idx)) {
+        keysTable(idx) = null
+        valsTable(idx) = null
+        size -= 1
+        return
+      } else {
+        idx += 1
+        idx &= mask
+        if (idx == init) {
+          return
+        }
+      }
+    }
+  }
+}
