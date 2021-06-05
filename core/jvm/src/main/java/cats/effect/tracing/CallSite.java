c15c9a687b8b9fe249bd0f73794135246fe84a3d
Vasil Vasilev <vasil@vasilev.io>
2021-06-05 10:17:37 +0200

    Remove unused imports

--- core/jvm/src/main/java/cats/effect/tracing/CallSite.java
+++ core/jvm/src/main/java/cats/effect/tracing/CallSite.java
@@ -19,8 +19,6 @@ package cats.effect.tracing;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
-import java.util.ArrayList;
-import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
