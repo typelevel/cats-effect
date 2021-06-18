7bbc9e97aa8be8fd0eebce5a675d9ce8403d042c
Vasil Vasilev <vasil@vasilev.io>
2021-06-18 21:00:54 +0200

    Add a default implementation for printStackTrace

--- std/shared/src/main/scala/cats/effect/std/Console.scala
+++ std/shared/src/main/scala/cats/effect/std/Console.scala
@@ -30,7 +30,7 @@ import cats.syntax.show._
 import scala.annotation.tailrec
 
 import java.lang.{StringBuilder => JStringBuilder}
-import java.io.EOFException
+import java.io.{ByteArrayOutputStream, EOFException, PrintStream}
 import java.nio.{ByteBuffer, CharBuffer}
 import java.nio.charset.{Charset, CodingErrorAction, MalformedInputException}
 
@@ -127,7 +127,12 @@ trait Console[F[_]] { self =>
   /**
    * Prints the stack trace of the given Throwable to standard error output.
    */
-  def printStackTrace(t: Throwable): F[Unit]
+  def printStackTrace(t: Throwable): F[Unit] = {
+    val baos = new ByteArrayOutputStream()
+    val ps = new PrintStream(baos)
+    t.printStackTrace(ps)
+    error(baos.toString)
+  }
 
   /**
    * Modifies the context in which this console operates using the natural
@@ -153,7 +158,7 @@ trait Console[F[_]] { self =>
       def errorln[A](a: A)(implicit S: Show[A]): G[Unit] =
         f(self.errorln(a))
 
-      def printStackTrace(t: Throwable): G[Unit] =
+      override def printStackTrace(t: Throwable): G[Unit] =
         f(self.printStackTrace(t))
     }
 }
@@ -329,7 +334,7 @@ object Console {
       F.blocking(System.err.println(text))
     }
 
-    def printStackTrace(t: Throwable): F[Unit] =
+    override def printStackTrace(t: Throwable): F[Unit] =
       F.blocking(t.printStackTrace())
   }
 }
