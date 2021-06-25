8d544c53e81ac8aa68b6257efecb941df250a222
Vasil Vasilev <vasil@vasilev.io>
2021-06-25 02:07:31 +0200

    Add OptionT guarantee test

--- tests/shared/src/test/scala/cats/effect/OptionTIOSpec.scala
+++ tests/shared/src/test/scala/cats/effect/OptionTIOSpec.scala
@@ -19,7 +19,9 @@ package cats.effect
 import cats.Order
 import cats.data.OptionT
 import cats.effect.laws.AsyncTests
+import cats.effect.syntax.all._
 import cats.laws.discipline.arbitrary._
+import cats.syntax.all._
 
 import org.scalacheck.Prop
 // import org.scalacheck.rng.Seed
@@ -40,6 +42,20 @@ class OptionTIOSpec
 
   // we just need this because of the laws testing, since the prop runs can interfere with each other
   sequential
+
+  "OptionT" should {
+    "execute finalizers for None" in ticked { implicit ticker =>
+      type F[A] = OptionT[IO, A]
+
+      val test = for {
+        gate <- Deferred[F, Unit]
+        _ <- OptionT.none[IO, Unit].guarantee(gate.complete(()).void).start
+        _ <- gate.get
+      } yield ()
+
+      test.value must completeAs(Some(()))
+    }
+  }
 
   implicit def ordOptionTIOFD(implicit ticker: Ticker): Order[OptionT[IO, FiniteDuration]] =
     Order by { ioaO => unsafeRun(ioaO.value).fold(None, _ => None, fa => fa) }
