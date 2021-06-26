0e08bb12ddd49f63aea827b9270ff6a1e5d7ce6f
Vasil Vasilev <vasil@vasilev.io>
2021-06-26 13:14:28 +0200

    Directly construct IOCont in IO.async

--- core/shared/src/main/scala/cats/effect/IO.scala
+++ core/shared/src/main/scala/cats/effect/IO.scala
@@ -1277,6 +1277,21 @@ object IO extends IOCompanionPlatform with IOLowPriorityImplicits {
   private[this] val _asyncForIO: kernel.Async[IO] = new kernel.Async[IO]
     with StackSafeMonad[IO] {
 
+    override def async[A](k: (Either[Throwable, A] => Unit) => IO[Option[IO[Unit]]]): IO[A] = {
+      val body = new Cont[IO, A, A] {
+        def apply[G[_]](implicit G: MonadCancel[G, Throwable]) = { (resume, get, lift) =>
+          G.uncancelable { poll =>
+            lift(k(resume)) flatMap {
+              case Some(fin) => G.onCancel(poll(get), lift(fin))
+              case None => poll(get)
+            }
+          }
+        }
+      }
+
+      IOCont(body, Tracing.calculateTracingEvent(k.getClass))
+    }
+
     override def async_[A](k: (Either[Throwable, A] => Unit) => Unit): IO[A] = {
       val body = new Cont[IO, A, A] {
         def apply[G[_]](implicit G: MonadCancel[G, Throwable]) = { (resume, get, lift) =>
