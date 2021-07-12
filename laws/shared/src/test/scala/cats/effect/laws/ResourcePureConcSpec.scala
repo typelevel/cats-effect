524836856eb0b124c7a1f6275990e14477ebc6f4
Vasil Vasilev <vasil@vasilev.io>
2021-07-13 01:49:09 +0200

    Move copy pasted code to kernel-testkit TestInstances

--- laws/shared/src/test/scala/cats/effect/laws/ResourcePureConcSpec.scala
+++ laws/shared/src/test/scala/cats/effect/laws/ResourcePureConcSpec.scala
@@ -17,23 +17,27 @@
 package cats.effect
 package laws
 
-import cats.Applicative
 import cats.effect.kernel.{MonadCancel, Resource}
-import cats.effect.kernel.testkit.{pure, OutcomeGenerators, PureConcGenerators}, pure._
+import cats.effect.kernel.testkit.{pure, OutcomeGenerators, PureConcGenerators, TestInstances}
 import cats.laws.discipline.arbitrary._
-import cats.kernel.Eq
 import cats.syntax.all._
 
-import org.scalacheck.{Arbitrary, Cogen, Gen, Prop}, Arbitrary.arbitrary
+import org.scalacheck.{Cogen, Prop}
 
 import org.specs2.ScalaCheck
 import org.specs2.mutable._
 
 import org.typelevel.discipline.specs2.mutable.Discipline
 
-class ResourcePureConcSpec extends Specification with Discipline with ScalaCheck with BaseSpec {
+class ResourcePureConcSpec
+    extends Specification
+    with Discipline
+    with ScalaCheck
+    with BaseSpec
+    with TestInstances {
   import PureConcGenerators._
   import OutcomeGenerators._
+  import pure._
 
   implicit def exec(sbool: Resource[PureConc[Throwable, *], Boolean]): Prop =
     Prop(
@@ -41,55 +45,6 @@ class ResourcePureConcSpec extends Specification with Discipline with ScalaCheck
         .run(sbool.use(_.pure[PureConc[Throwable, *]]))
         .fold(false, _ => false, fb => fb.fold(false)(identity))
     )
-
-  //////////// Copy-pasted code
-  implicit def arbitraryResource[F[_], A](
-      implicit F: Applicative[F],
-      AFA: Arbitrary[F[A]],
-      AFU: Arbitrary[F[Unit]],
-      AA: Arbitrary[A]
-  ): Arbitrary[Resource[F, A]] =
-    Arbitrary(Gen.delay(genResource[F, A]))
-
-  // Consider improving this a strategy similar to Generators.
-  // Doesn't include interruptible resources
-  def genResource[F[_], A](
-      implicit F: Applicative[F],
-      AFA: Arbitrary[F[A]],
-      AFU: Arbitrary[F[Unit]],
-      AA: Arbitrary[A]
-  ): Gen[Resource[F, A]] = {
-    def genAllocate: Gen[Resource[F, A]] =
-      for {
-        alloc <- arbitrary[F[A]]
-        dispose <- arbitrary[F[Unit]]
-      } yield Resource(alloc.map(a => a -> dispose))
-
-    def genBind: Gen[Resource[F, A]] =
-      genAllocate.map(_.flatMap(a => Resource.pure[F, A](a)))
-
-    def genEval: Gen[Resource[F, A]] =
-      arbitrary[F[A]].map(Resource.eval)
-
-    def genPure: Gen[Resource[F, A]] =
-      arbitrary[A].map(Resource.pure)
-
-    Gen.frequency(
-      5 -> genAllocate,
-      1 -> genBind,
-      1 -> genEval,
-      1 -> genPure
-    )
-  }
-
-  implicit def eqResource[F[_], A](
-      implicit E: Eq[F[A]],
-      F: MonadCancel[F, Throwable]): Eq[Resource[F, A]] =
-    new Eq[Resource[F, A]] {
-      def eqv(x: Resource[F, A], y: Resource[F, A]): Boolean =
-        E.eqv(x.use(F.pure), y.use(F.pure))
-    }
-  //////////// End of copy-pasted code
 
   implicit def cogenForResource[F[_], A](
       implicit C: Cogen[F[(A, F[Unit])]],
