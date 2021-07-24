31c18f59f5ec64eeb90abb526ab173b117f054b2
Arman Bilge <armanbilge@gmail.com>
2021-07-24 11:34:14 -0700

    Add `flatTap` to `IO`

--- core/shared/src/main/scala/cats/effect/IO.scala
+++ core/shared/src/main/scala/cats/effect/IO.scala
@@ -347,6 +347,8 @@ sealed abstract class IO[+A] private () extends IOPlatform[A] {
     IO.FlatMap(this, f, Tracing.calculateTracingEvent(f.getClass))
 
   def flatten[B](implicit ev: A <:< IO[B]): IO[B] = flatMap(ev)
+  
+  def flatTap[B](f: A => IO[B]): IO[A] = flatMap(a => f(a).as(a))
 
   /**
    * Executes the given `finalizer` when the source is finished,
