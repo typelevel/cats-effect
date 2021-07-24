25cc509ab6ef4e1fc0f2333c7709da6c96bc7c8c
Arman Bilge <armanbilge@gmail.com>
2021-07-24 21:01:52 +0000

    More efficient IO.foreverM
    Co-authored-by: Vasil Vasilev <vasil@vasilev.io>

--- core/shared/src/main/scala/cats/effect/IO.scala
+++ core/shared/src/main/scala/cats/effect/IO.scala
@@ -813,7 +813,7 @@ sealed abstract class IO[+A] private () extends IOPlatform[A] {
     interpret(this)
   }
 
-  def foreverM: IO[Nothing] = Monad[IO].foreverM[A, Nothing](this)
+  def foreverM: IO[Nothing] = IO.asyncForIO.foreverM[A, Nothing](this)
 
   def whileM[G[_]: Alternative, B >: A](p: IO[Boolean]): IO[G[B]] =
     Monad[IO].whileM[G, B](p)(this)
