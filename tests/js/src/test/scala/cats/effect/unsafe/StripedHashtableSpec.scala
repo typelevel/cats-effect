3a0820f1b95e3909a5371fb67ec0680ce231b5c4
Arman Bilge <armanbilge@gmail.com>
2021-08-16 13:50:28 +0000

    Increase timeouts

--- tests/js/src/test/scala/cats/effect/unsafe/StripedHashtableSpec.scala
+++ tests/js/src/test/scala/cats/effect/unsafe/StripedHashtableSpec.scala
@@ -24,7 +24,7 @@ import scala.concurrent.duration._
 
 class StripedHashtableSpec extends BaseSpec with Runners {
 
-  override def executionTimeout: FiniteDuration = 30.seconds
+  override def executionTimeout: FiniteDuration = 1.minute
 
   def hashtableRuntime(): IORuntime =
     new IORuntime(
