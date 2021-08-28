150c0afe75d4de37e4aadb1a042f4c9f85c4a756
Vasil Vasilev <vasil@vasilev.io>
2021-08-28 10:30:33 +0200

    Adjust the  comment on the local queue capacity check when draining a batch

--- core/jvm/src/main/scala/cats/effect/unsafe/LocalQueue.scala
+++ core/jvm/src/main/scala/cats/effect/unsafe/LocalQueue.scala
@@ -680,8 +680,8 @@ private final class LocalQueue {
       val real = lsb(hd)
 
       if (unsignedShortSubtraction(tl, real) < OverflowBatchSize) {
-        // The tail and the "real" value of the head are equal. The queue is
-        // empty. There is nothing more to be done.
+        // The current remaining capacity of the local queue is enough to
+        // accommodate the new incoming batch. There is nothing more to be done.
         return
       }
 
