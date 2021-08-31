a870d564de79a78ac78e53c12919f8f2f4a5c5cd
Arman Bilge <armanbilge@gmail.com>
2021-08-31 14:57:29 +0000

    Increase another timeout

--- tests/shared/src/test/scala/cats/effect/std/PQueueSpec.scala
+++ tests/shared/src/test/scala/cats/effect/std/PQueueSpec.scala
@@ -29,8 +29,11 @@ import org.scalacheck.Arbitrary.arbitrary
 import org.specs2.specification.core.Fragments
 
 import scala.collection.immutable.{Queue => ScalaQueue}
+import scala.concurrent.duration._
 
 class BoundedPQueueSpec extends BaseSpec with PQueueTests {
+
+  override def executionTimeout = 20.seconds
 
   implicit val orderForInt: Order[Int] = Order.fromLessThan((x, y) => x < y)
 
