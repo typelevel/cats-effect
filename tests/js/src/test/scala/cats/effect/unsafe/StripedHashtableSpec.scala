35df0f28b42fd72a87f52f208a5db01b1c9c8774
Vasil Vasilev <vasil@vasilev.io>
2021-09-04 17:07:00 +0200

    Remove unnecessary trait extensions

--- tests/js/src/test/scala/cats/effect/unsafe/StripedHashtableSpec.scala
+++ tests/js/src/test/scala/cats/effect/unsafe/StripedHashtableSpec.scala
@@ -22,7 +22,7 @@ import cats.syntax.traverse._
 import scala.concurrent.{Future, Promise}
 import scala.concurrent.duration._
 
-class StripedHashtableSpec extends BaseSpec with Runners {
+class StripedHashtableSpec extends BaseSpec {
 
   override def executionTimeout: FiniteDuration = 2.minutes
 
