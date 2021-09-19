6d0727d8b703b6837c2d93ae7dc424e7b0872579
Christopher Davenport <chris@christopherdavenport.tech>
2021-09-18 19:18:53 -0700

    scalafmt

--- kernel/shared/src/main/scala/cats/effect/kernel/Outcome.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/Outcome.scala
@@ -166,8 +166,8 @@ object Outcome extends LowPriorityImplicits {
    * embedError allows the restoration to a normal development flow from an Outcome.
    *
    * This can be useful for storing the state of a running computation and then waiters for that
-   * data can act and continue forward on that shared outcome. Cancelation is encoded
-   * as a `CancellationException`.
+   * data can act and continue forward on that shared outcome. Cancelation is encoded as a
+   * `CancellationException`.
    */
   def embedError[F[_], A](outcome: Outcome[F, Throwable, A])(
       implicit F: MonadCancel[F, Throwable]): F[A] =
