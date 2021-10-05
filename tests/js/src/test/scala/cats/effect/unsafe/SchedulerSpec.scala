5908f77cb410fe6fe1f7894ef2036dfd43ee55bd
Arman Bilge <armanbilge@gmail.com>
2021-10-05 05:12:31 +0000

    Add test for JS TimeoutOverflowWarning

---
+++ tests/js/src/test/scala/cats/effect/unsafe/SchedulerSpec.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020-2021 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect
+package unsafe
+
+import scala.concurrent.duration._
+
+class SchedulerSpec extends BaseSpec {
+
+  "Default scheduler" should {
+    "correctly handle very long sleeps" in real {
+      IO.sleep(Long.MaxValue.nanos).race(IO.sleep(1.second)) mustEqual Right(())
+    }
+  }
+
+}
