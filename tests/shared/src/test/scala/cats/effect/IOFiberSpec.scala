c3c134ae99e45dfb5a8af12b82439a070d8abea8
Arman Bilge <armanbilge@gmail.com>
2021-10-15 11:41:46 -0700

    Fix CI

--- tests/shared/src/test/scala/cats/effect/IOFiberSpec.scala
+++ tests/shared/src/test/scala/cats/effect/IOFiberSpec.scala
@@ -29,15 +29,17 @@ class IOFiberSpec extends BaseSpec {
 
     "toString a running fiber" in real {
       def loop: IO[Unit] = IO.unit >> loop
-      val pattern =
+      val pattern1 =
         raw"cats.effect.IOFiber@[0-9a-f][0-9a-f]+ RUNNING >> @ fiber.IOFiberSpec.loop\$$1\(.*IOFiberSpec.scala:[0-9]{2}\)"
+      val pattern2 =
+        raw"cats.effect.IOFiber@[0-9a-f][0-9a-f]+ RUNNING flatMap @ cats.effect.IO.$$greater$$greater\(.*IOFiberSpec.scala:[0-9]{2}\)"
       for {
         f <- loop.start
         _ <- IO.sleep(1.milli)
         s <- IO(f.toString)
         _ <- IO.println(s)
         _ <- f.cancel
-      } yield s.matches(pattern)
+      } yield s.matches(s"($pattern1)|($pattern2)")
     }
 
     "toString a suspended fiber" in real {
