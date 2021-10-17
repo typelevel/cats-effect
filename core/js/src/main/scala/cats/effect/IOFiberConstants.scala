ded5b4d59d783cbdc26724cc32791a0b3740d69e
Vasil Vasilev <vasil@vasilev.io>
2021-10-17 22:23:33 +0200

    Make JS constants final

--- core/js/src/main/scala/cats/effect/IOFiberConstants.scala
+++ core/js/src/main/scala/cats/effect/IOFiberConstants.scala
@@ -17,44 +17,44 @@
 package cats.effect
 
 // defined in Java for the JVM, Scala for ScalaJS (where object field access is faster)
-private[effect] object IOFiberConstants {
+private object IOFiberConstants {
 
-  val MaxStackDepth: Int = 512
+  final val MaxStackDepth: Int = 512
 
   /*
    * allow for 255 masks before conflicting; 255 chosen because it is a familiar bound,
    * and because it's evenly divides UnsignedInt.MaxValue.
    * This scheme gives us 16,843,009 (~2^24) potential derived fibers before masks can conflict
    */
-  val ChildMaskOffset: Int = 255
+  final val ChildMaskOffset: Int = 255
 
   // continuation ids (should all be inlined)
-  val MapK: Byte = 0
-  val FlatMapK: Byte = 1
-  val CancelationLoopK: Byte = 2
-  val RunTerminusK: Byte = 3
-  val EvalOnK: Byte = 4
-  val HandleErrorWithK: Byte = 5
-  val OnCancelK: Byte = 6
-  val UncancelableK: Byte = 7
-  val UnmaskK: Byte = 8
-  val AttemptK: Byte = 9
+  final val MapK: Byte = 0
+  final val FlatMapK: Byte = 1
+  final val CancelationLoopK: Byte = 2
+  final val RunTerminusK: Byte = 3
+  final val EvalOnK: Byte = 4
+  final val HandleErrorWithK: Byte = 5
+  final val OnCancelK: Byte = 6
+  final val UncancelableK: Byte = 7
+  final val UnmaskK: Byte = 8
+  final val AttemptK: Byte = 9
 
   // resume ids
-  val ExecR: Byte = 0
-  val AsyncContinueSuccessfulR: Byte = 1
-  val AsyncContinueFailedR: Byte = 2
-  val BlockingR: Byte = 3
-  val AfterBlockingSuccessfulR: Byte = 4
-  val AfterBlockingFailedR: Byte = 5
-  val EvalOnR: Byte = 6
-  val CedeR: Byte = 7
-  val AutoCedeR: Byte = 8
-  val DoneR: Byte = 9
+  final val ExecR: Byte = 0
+  final val AsyncContinueSuccessfulR: Byte = 1
+  final val AsyncContinueFailedR: Byte = 2
+  final val BlockingR: Byte = 3
+  final val AfterBlockingSuccessfulR: Byte = 4
+  final val AfterBlockingFailedR: Byte = 5
+  final val EvalOnR: Byte = 6
+  final val CedeR: Byte = 7
+  final val AutoCedeR: Byte = 8
+  final val DoneR: Byte = 9
 
   // ContState tags
-  val ContStateInitial: Int = 0
-  val ContStateWaiting: Int = 1
-  val ContStateWinner: Int = 2
-  val ContStateResult: Int = 3
+  final val ContStateInitial: Int = 0
+  final val ContStateWaiting: Int = 1
+  final val ContStateWinner: Int = 2
+  final val ContStateResult: Int = 3
 }
