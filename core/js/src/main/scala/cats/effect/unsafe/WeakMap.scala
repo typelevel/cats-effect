b231ed46cd7051e343bd213d70a9d7a7f622f407
Arman Bilge <armanbilge@gmail.com>
2021-10-19 02:16:57 +0000

    Create WeakMap facade

---
+++ core/js/src/main/scala/cats/effect/unsafe/WeakMap.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020-2021 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect.unsafe
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+import scala.annotation.nowarn
+
+@js.native
+@JSGlobal
+@nowarn("cat=unused")
+private[unsafe] class WeakMap[K, V] extends js.Object {
+  def delete(key: K): Boolean = js.native
+  def get(key: K): js.UndefOr[V] = js.native
+  def has(key: K): Boolean = js.native
+  def set(key: K, value: V): this.type = js.native
+}
