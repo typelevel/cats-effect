0a92c7ee7fe34846d0b1d0040430225c3083ec6b
Vasil Vasilev <vasil@vasilev.io>
2021-11-15 16:02:39 +0100

    Reflectively register signal handling if available

--- core/jvm/src/main/java/cats/effect/Signal.java
+++ core/jvm/src/main/java/cats/effect/Signal.java
@@ -16,29 +16,75 @@
 
 package cats.effect;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
 import java.util.function.Consumer;
 
 final class Signal {
 
-  private static final SignalHandler SIGNAL_HANDLER = new SunMiscSignalHandler();
+  private static final SignalHandler SIGNAL_HANDLER = initSignalHandler();
 
-  static void handleSignal(String signal, Consumer<Object> handler)
-      throws IllegalArgumentException {
+  static void handleSignal(String signal, Consumer<Object> handler) {
     SIGNAL_HANDLER.handleSignal(signal, handler);
   }
 
   abstract static class SignalHandler {
-    abstract void handleSignal(String signal, Consumer<Object> handler)
-        throws IllegalArgumentException;
+    abstract void handleSignal(String signal, Consumer<Object> handler);
   }
 
-  static final class SunMiscSignalHandler extends SignalHandler {
-    void handleSignal(String signal, Consumer<Object> handler) throws IllegalArgumentException {
-      sun.misc.Signal.handle(new sun.misc.Signal(signal), handler::accept);
+  static final SignalHandler initSignalHandler() {
+    try {
+      final Class<?> sunMiscSignalClass = Class.forName("sun.misc.Signal");
+      final Class<?> sunMiscSignalHandlerClass = Class.forName("sun.misc.SignalHandler");
+      final MethodHandles.Lookup lookup = MethodHandles.lookup();
+      final MethodType signalConstructorMethodType =
+          MethodType.methodType(void.class, String.class);
+      final MethodHandle signalConstructorMethodHandle =
+          lookup.findConstructor(sunMiscSignalClass, signalConstructorMethodType);
+      final MethodType handleMethodType =
+          MethodType.methodType(
+              sunMiscSignalHandlerClass, sunMiscSignalClass, sunMiscSignalHandlerClass);
+      final MethodHandle handleMethodHandle =
+          lookup.findStatic(sunMiscSignalClass, "handle", handleMethodType);
+
+      return new SignalHandler() {
+        final void handleSignal(String signal, Consumer<Object> handler) {
+          final InvocationHandler invocationHandler = invocationHandlerFromConsumer(handler);
+          final Object proxy =
+              Proxy.newProxyInstance(
+                  Signal.class.getClassLoader(),
+                  new Class<?>[] {sunMiscSignalHandlerClass},
+                  invocationHandler);
+
+          try {
+            final Object s = signalConstructorMethodHandle.invoke(signal);
+            handleMethodHandle.invoke(s, proxy);
+          } catch (Throwable t) {
+            // TODO: Maybe t.printStackTrace() ?
+            return; // Gracefully degrade to a no-op.
+          }
+        }
+      };
+    } catch (Throwable t) {
+      // TODO: Maybe t.printStackTrace() ?
+
+      // Gracefully degrade to a no-op implementation.
+      return new SignalHandler() {
+        final void handleSignal(String signal, Consumer<Object> handler) {}
+      };
     }
   }
 
-  static final class NoOpSignalHandler extends SignalHandler {
-    void handleSignal(String signal, Consumer<Object> handler) throws IllegalArgumentException {}
+  static final InvocationHandler invocationHandlerFromConsumer(Consumer<Object> consumer) {
+    return (proxy, method, args) -> {
+      if (method.getName().equals("handle")) {
+        consumer.accept(args[0]);
+        return null;
+      }
+      return null;
+    };
   }
 }
