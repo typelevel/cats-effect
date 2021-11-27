c643381ba427e9cd2a4e07461aaaf2d8c7643676
Arman Bilge <armanbilge@gmail.com>
2021-11-27 17:32:21 +0000

    Only register main fiber if stacktracing

--- core/jvm/src/main/scala/cats/effect/IOApp.scala
+++ core/jvm/src/main/scala/cats/effect/IOApp.scala
@@ -279,7 +279,8 @@ trait IOApp {
           latch.countDown()
         })(runtime)
 
-    runtime.fiberMonitor.monitorSuspended(fiber, fiber)
+    if (isStackTracing)
+      runtime.fiberMonitor.monitorSuspended(fiber, fiber)
 
     def handleShutdown(): Unit = {
       if (latch.getCount() > 0) {
