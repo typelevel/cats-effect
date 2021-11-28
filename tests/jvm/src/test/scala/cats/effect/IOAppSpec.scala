067841300b46882b0019b0d18bb4637af76bc9cd
Vasil Vasilev <vasil@vasilev.io>
2021-11-29 00:11:57 +0100

    Halt the runtime on fatal errors

--- tests/jvm/src/test/scala/cats/effect/IOAppSpec.scala
+++ tests/jvm/src/test/scala/cats/effect/IOAppSpec.scala
@@ -230,6 +230,13 @@ class IOAppSpec extends Specification {
           h.awaitStatus() mustEqual 0
         }
 
+        "shutdown on worker thread interruption" in {
+          val h = platform(WorkerThreadInterrupt, List.empty)
+          h.awaitStatus() mustEqual 1
+          h.stderr() must contain("java.lang.InterruptedException")
+          ok
+        }
+
         if (!BuildInfo.testJSIOApp && sys
             .props
             .get("java.version")
