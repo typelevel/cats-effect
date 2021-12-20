d8486cff4ef94ecaf895325e8123391f2eda280c
Vasil Vasilev <vasil@vasilev.io>
2021-12-20 12:21:16 +0100

    Implement a simple synchronized WeakBag as a fallback

---
+++ core/jvm/src/main/scala/cats/effect/unsafe/SynchronizedWeakBag.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020-2021 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect.unsafe
+
+private final class SynchronizedWeakBag[A <: AnyRef] {
+  import WeakBag.Handle
+
+  private[this] val weakBag: WeakBag[A] = new WeakBag()
+
+  def insert(a: A): Handle = synchronized {
+    weakBag.insert(a)
+  }
+
+  def toSet: Set[A] = synchronized {
+    weakBag.toSet
+  }
+
+  def size: Int = synchronized {
+    weakBag.size
+  }
+}
