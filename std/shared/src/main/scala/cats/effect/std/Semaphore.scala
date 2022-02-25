82486945b3e6ba47c619239b8054586980377482
Tim Spence <timothywspence@gmail.com>
2022-02-25 16:13:52 +0000

    Scalafmt

--- std/shared/src/main/scala/cats/effect/std/Semaphore.scala
+++ std/shared/src/main/scala/cats/effect/std/Semaphore.scala
@@ -255,7 +255,9 @@ object Semaphore {
           Resource.makeFull { (poll: Poll[F]) => poll(acquire) } { _ => release }
 
         def tryPermit: Resource[F, Boolean] =
-          Resource.makeFull { (poll: Poll[F]) => poll(tryAcquire) } { acquired => release.whenA(acquired) }
+          Resource.makeFull { (poll: Poll[F]) => poll(tryAcquire) } { acquired =>
+            release.whenA(acquired)
+          }
 
         def tryAcquireN(n: Long): F[Boolean] = {
           requireNonNegative(n)
