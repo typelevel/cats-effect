00c49429cbc8b8a99669c09733e0d8d0666cccf9
Daniel Spiewak <djspiewak@gmail.com>
2022-04-03 15:03:57 -0600

    Update site for 3.3.10

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -57,10 +57,10 @@ class HomeSplash extends React.Component {
                 <div className="inner">
                     <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
                     <code className="hljs css language-sbt">
-                        <span className="hljs-string">"org.typelevel"</span> %% <span className="hljs-string">"cats-effect"</span> % <span className="hljs-string">"3.3.9"</span>
+                        <span className="hljs-string">"org.typelevel"</span> %% <span className="hljs-string">"cats-effect"</span> % <span className="hljs-string">"3.3.10"</span>
                     </code>
                     <PromoSection>
-                        <Button target="_blank" href="https://scastie.scala-lang.org/ak8sbVPGSxKEbOmyvcOUNg">Try It!</Button>
+                        <Button target="_blank" href="https://scastie.scala-lang.org/NGMkhrrER9W0rM7it8JyEA">Try It!</Button>
                         <Button href={docUrl('getting-started')}>Get Started</Button>
                     </PromoSection>
                 </div>
