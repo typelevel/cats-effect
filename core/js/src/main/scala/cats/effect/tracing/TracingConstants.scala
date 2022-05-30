1bbc0fc47bf2f7f697f6316bb2febee1047f8f62
Arman Bilge <armanbilge@gmail.com>
2022-05-29 19:11:14 -0700

    Disable tracing by default on JS

--- core/js/src/main/scala/cats/effect/tracing/TracingConstants.scala
+++ core/js/src/main/scala/cats/effect/tracing/TracingConstants.scala
@@ -20,7 +20,7 @@ package tracing
 private[effect] object TracingConstants {
 
   private[this] final val stackTracingMode: String =
-    process.env("CATS_EFFECT_TRACING_MODE").filterNot(_.isEmpty).getOrElse("cached")
+    process.env("CATS_EFFECT_TRACING_MODE").filterNot(_.isEmpty).getOrElse("none")
 
   final val isCachedStackTracing: Boolean = stackTracingMode.equalsIgnoreCase("cached")
 
