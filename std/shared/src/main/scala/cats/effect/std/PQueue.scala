46b3af32a7d5ace98f2d1ddb8f724fb09a3fe732
Daniel Spiewak <djspiewak@gmail.com>
2022-06-18 15:59:29 -0600

    Fixed `tryTake` and `tryTakeN` for `Dequeue` and `PQueue`

--- std/shared/src/main/scala/cats/effect/std/PQueue.scala
+++ std/shared/src/main/scala/cats/effect/std/PQueue.scala
@@ -170,10 +170,6 @@ object PQueue {
             val (release, tail) = offerers.dequeue
             State(rest, size - 1, takers, tail) -> release.complete(()).as(a.some)
 
-          case State(heap, size, takers, offerers) if offerers.nonEmpty =>
-            val (release, rest) = offerers.dequeue
-            State(heap, size, takers, rest) -> (release.complete(()) *> tryTake)
-
           case s =>
             s -> F.pure(none[A])
         }
@@ -272,17 +268,19 @@ trait PQueueSource[F[_], A] {
    */
   def tryTakeN(maxN: Option[Int])(implicit F: Monad[F]): F[List[A]] = {
     PQueueSource.assertMaxNPositive(maxN)
-    F.tailRecM[(List[A], Int), List[A]](
-      (List.empty[A], 0)
-    ) {
-      case (list, i) =>
-        if (maxN.contains(i)) list.reverse.asRight.pure[F]
-        else {
-          tryTake.map {
-            case None => list.reverse.asRight
-            case Some(x) => (x +: list, i + 1).asLeft
-          }
+
+    def loop(i: Int, limit: Int, acc: List[A]): F[List[A]] =
+      if (i >= limit)
+        F.pure(acc.reverse)
+      else
+        tryTake flatMap {
+          case Some(a) => loop(i + 1, limit, a :: acc)
+          case None => F.pure(acc.reverse)
         }
+
+    maxN match {
+      case Some(limit) => loop(0, limit, Nil)
+      case None => loop(0, Int.MaxValue, Nil)
     }
   }
 
