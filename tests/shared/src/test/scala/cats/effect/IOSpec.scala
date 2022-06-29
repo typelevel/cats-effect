b73e726b5c77129319959c4d8710462fd0b8b633
Arman Bilge <armanbilge@gmail.com>
2022-06-29 16:23:38 +0000

    Add test for `syncStep`ing `uncancelable`

--- tests/shared/src/test/scala/cats/effect/IOSpec.scala
+++ tests/shared/src/test/scala/cats/effect/IOSpec.scala
@@ -1455,6 +1455,11 @@ class IOSpec extends BaseSpec with Discipline with IOPlatformSpecification {
           i must beEqualTo(1)
       }
 
+      "handle uncancelable" in {
+        val sio = IO.unit.uncancelable.syncStep
+        sio.map(_.bimap(_ => (), _ => ())) must completeAsSync(Right(()))
+      }
+
       "synchronously allocate a vanilla resource" in {
         val sio = Resource.make(IO.unit)(_ => IO.unit).allocated.map(_._1).syncStep
         sio.map(_.bimap(_ => (), _ => ())) must completeAsSync(Right(()))
