7efbac0eb49239252b556db5c0b7abaa11997ea5
Arman Bilge <armanbilge@gmail.com>
2022-08-24 03:50:24 +0000

    Make `installGlobal` public

--- core/native/src/main/scala/cats/effect/unsafe/IORuntimeCompanionPlatform.scala
+++ core/native/src/main/scala/cats/effect/unsafe/IORuntimeCompanionPlatform.scala
@@ -26,7 +26,7 @@ private[unsafe] abstract class IORuntimeCompanionPlatform { this: IORuntime.type
 
   private[this] var _global: IORuntime = null
 
-  private[effect] def installGlobal(global: => IORuntime): Boolean = {
+  def installGlobal(global: => IORuntime): Boolean = {
     if (_global == null) {
       _global = global
       true
