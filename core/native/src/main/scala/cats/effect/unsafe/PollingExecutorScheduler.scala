989ad9ca450e16a0ed7302203a13036f98d520e3
Arman Bilge <armanbilge@gmail.com>
2022-09-06 03:09:40 +0000

    More cleanup for `nowMicros`

--- core/native/src/main/scala/cats/effect/unsafe/PollingExecutorScheduler.scala
+++ core/native/src/main/scala/cats/effect/unsafe/PollingExecutorScheduler.scala
@@ -68,10 +68,11 @@ abstract class PollingExecutorScheduler extends ExecutionContextExecutor with Sc
   override def nowMicros(): Long =
     if (LinktimeInfo.isFreeBSD || LinktimeInfo.isLinux || LinktimeInfo.isMac) {
       import scala.scalanative.posix.time._
-      val tp = stackalloc[timespec]()
-      if (clock_gettime(CLOCK_REALTIME, tp) != 0)
+      import scala.scalanative.posix.timeOps._
+      val ts = stackalloc[timespec]()
+      if (clock_gettime(CLOCK_REALTIME, ts) != 0)
         throw new RuntimeException(s"clock_gettime: ${errno.errno}")
-      tp._1 * 1000000 + tp._2 / 1000
+      ts.tv_sec * 1000000 + ts.tv_nsec / 1000
     } else {
       super.nowMicros()
     }
