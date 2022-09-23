61ce5e6c46dc255280ab7f7c04007915802b23e0
Tim Spence <timothywspence@gmail.com>
2022-09-23 17:30:37 +0100

    Actually calculate fibonacci

--- tests/shared/src/main/scala/catseffect/examples.scala
+++ tests/shared/src/main/scala/catseffect/examples.scala
@@ -111,7 +111,7 @@ package examples {
   object CpuStarvation extends IOApp.Simple {
 
     def fib(n: Long): Long = n match {
-      case 0 => 0
+      case 0 => 1
       case 1 => 1
       case n => fib(n - 1) + fib(n - 2)
     }
