c1eb1511b7e59d7d00c4785337765c9c75d4a135
Alexey Shuksto <a.shuksto@kryptonite.ru>
2022-11-21 15:57:04 +0300

    Fix 'Dispatcher' deprecaton annotation 'message' and 'since'

--- std/shared/src/main/scala/cats/effect/std/Dispatcher.scala
+++ std/shared/src/main/scala/cats/effect/std/Dispatcher.scala
@@ -96,8 +96,9 @@ object Dispatcher {
   private[this] val Completed: Either[Throwable, Unit] = Right(())
 
   @deprecated(
-    "3.4.0",
-    "use parallel or sequential instead; the former corresponds to the current semantics of this method")
+    since = "3.4.0",
+    message =
+      "use '.parallel' or '.sequential' instead; the former corresponds to the current semantics of '.apply'")
   def apply[F[_]: Async]: Resource[F, Dispatcher[F]] = parallel[F](await = false)
 
   /**
