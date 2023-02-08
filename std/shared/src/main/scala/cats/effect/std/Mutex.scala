e1ac43ffd59a38de6fd4b05d2cc418d7dfff8b53
Arman Bilge <armanbilge@gmail.com>
2023-02-08 04:55:16 +0000

    Implement async mutex with `UnsafeUnbounded`

--- std/shared/src/main/scala/cats/effect/std/Mutex.scala
+++ std/shared/src/main/scala/cats/effect/std/Mutex.scala
@@ -21,7 +21,7 @@ package std
 import cats.effect.kernel._
 import cats.syntax.all._
 
-import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * A purely functional mutex.
@@ -84,9 +84,7 @@ object Mutex {
    * Creates a new `Mutex`. Like `apply` but initializes state using another effect constructor.
    */
   def in[F[_], G[_]](implicit F: Sync[F], G: Async[G]): F[Mutex[G]] =
-    F.delay(
-      new AtomicReference[LockCell]()
-    ).map(state => new AsyncImpl[G](state)(G))
+    F.delay(new AsyncImpl[G])
 
   private final class ConcurrentImpl[F[_]](sem: Semaphore[F]) extends Mutex[F] {
     override final val lock: Resource[F, Unit] =
@@ -96,87 +94,38 @@ object Mutex {
       new ConcurrentImpl(sem.mapK(f))
   }
 
-  private final class AsyncImpl[F[_]](state: AtomicReference[LockCell])(implicit F: Async[F])
-      extends Mutex[F] {
-    // Cancels a Fiber waiting for the Mutex.
-    private def cancel(thisCB: CB, thisCell: LockCell, previousCell: LockCell): F[Unit] =
+  private final class AsyncImpl[F[_]](implicit F: Async[F]) extends Mutex[F] {
+    private[this] val locked = new AtomicBoolean(false)
+    private[this] val waiters = new UnsafeUnbounded[Either[Throwable, Unit] => Unit]
+
+    private[this] val acquire: F[Unit] = F.asyncCheckAttempt { cb =>
       F.delay {
-        // If we are canceled.
-        // First, we check if the state still contains ourselves,
-        // if that is the case, we swap it with the previousCell.
-        // This ensures any consequent attempt to acquire the Mutex
-        // will register its callback on the appropriate cell.
-        // Additionally, that confirms there is no Fiber
-        // currently waiting for us.
-        if (!state.compareAndSet(thisCell, previousCell)) {
-          // Otherwise,
-          // it means we have a Fiber waiting for us.
-          // Thus, we need to tell the previous cell
-          // to awake that Fiber instead.
-          var nextCB = thisCell.get()
-          while (nextCB eq null) {
-            // There is a tiny fraction of time when
-            // the next cell has acquired ourselves,
-            // but hasn't registered itself yet.
-            // Thus, we spin loop until that happens
-            nextCB = thisCell.get()
-          }
-          if (!previousCell.compareAndSet(thisCB, nextCB)) {
-            // However, in case the previous cell had already completed,
-            // then the Mutex is free and we can awake our waiting fiber.
-            if (nextCB ne null) nextCB.apply(Either.unit)
-          }
-        }
-      }
-
-    // Awaits until the Mutex is free.
-    private def await(thisCell: LockCell): F[Unit] =
-      F.asyncCheckAttempt[Unit] { thisCB =>
-        F.delay {
-          val previousCell = state.getAndSet(thisCell)
-
-          if (previousCell eq null) {
-            // If the previous cell was null,
-            // then the Mutex is free.
+        if (locked.compareAndSet(false, true)) { // acquired
+          Either.unit
+        } else {
+          val cancel = waiters.put(cb)
+          if (locked.compareAndSet(false, true)) { // try again
+            cancel()
             Either.unit
           } else {
-            // Otherwise,
-            // we check again that the previous cell haven't been completed yet,
-            // if not we tell the previous cell to awake us when they finish.
-            if (!previousCell.compareAndSet(null, thisCB)) {
-              // If it was already completed,
-              // then the Mutex is free.
-              Either.unit
-            } else {
-              Left(Some(cancel(thisCB, thisCell, previousCell)))
-            }
+            Left(Some(F.delay(cancel())))
           }
         }
       }
+    }
 
-    // Acquires the Mutex.
-    private def acquire(poll: Poll[F]): F[LockCell] =
-      F.delay(new AtomicReference[CB]()).flatMap { thisCell =>
-        poll(await(thisCell).map(_ => thisCell))
+    private[this] val _release: F[Unit] = F.delay {
+      try { // pass the buck
+        waiters.take().apply(Either.unit)
+      } catch { // release
+        case FailureSignal => locked.set(false)
       }
+    }
 
-    // Releases the Mutex.
-    private def release(thisCell: LockCell): F[Unit] =
-      F.delay {
-        // If the state still contains our own cell,
-        // then it means nobody was waiting for the Mutex,
-        // and thus it can be put on a free state again.
-        if (!state.compareAndSet(thisCell, null)) {
-          // Otherwise,
-          // our cell is probably not empty,
-          // we must awake whatever Fiber is waiting for us.
-          val nextCB = thisCell.getAndSet(Sentinel)
-          if (nextCB ne null) nextCB.apply(Either.unit)
-        }
-      }
+    private[this] val release: Unit => F[Unit] = _ => _release
 
     override final val lock: Resource[F, Unit] =
-      Resource.makeFull[F, LockCell](acquire)(release).void
+      Resource.makeFull[F, Unit](poll => poll(acquire))(release)
 
     override def mapK[G[_]](f: F ~> G)(implicit G: MonadCancel[G, _]): Mutex[G] =
       new Mutex.TransformedMutex(this, f)
@@ -194,9 +143,4 @@ object Mutex {
       new Mutex.TransformedMutex(this, f)
   }
 
-  private type CB = Either[Throwable, Unit] => Unit
-
-  private final val Sentinel: CB = _ => ()
-
-  private type LockCell = AtomicReference[CB]
 }
