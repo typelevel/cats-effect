27aa9c89b0015f4f3cabcca0a1195a8507216b1c
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2023-04-26 12:48:53 -0500

    Revert AsyncMutex implementation to the LockChain based one

--- std/shared/src/main/scala/cats/effect/std/Mutex.scala
+++ std/shared/src/main/scala/cats/effect/std/Mutex.scala
@@ -21,8 +21,6 @@ package std
 import cats.effect.kernel._
 import cats.syntax.all._
 
-import java.util.concurrent.atomic.AtomicReference
-
 /**
  * A purely functional mutex.
  *
@@ -84,9 +82,7 @@ object Mutex {
    * Creates a new `Mutex`. Like `apply` but initializes state using another effect constructor.
    */
   def in[F[_], G[_]](implicit F: Sync[F], G: Async[G]): F[Mutex[G]] =
-    F.delay(
-      new AtomicReference[AsyncImpl.LockCell]()
-    ).map(state => new AsyncImpl[G](state)(G))
+    Ref.in[F, G, AsyncImpl.LockChain](AsyncImpl.Empty).map(state => new AsyncImpl[G](state))
 
   private final class ConcurrentImpl[F[_]](sem: Semaphore[F]) extends Mutex[F] {
     override final val lock: Resource[F, Unit] =
@@ -97,114 +93,38 @@ object Mutex {
   }
 
   private final class AsyncImpl[F[_]](
-      state: AtomicReference[AsyncImpl.LockCell]
+      state: Ref[F, AsyncImpl.LockChain]
   )(
       implicit F: Async[F]
   ) extends Mutex[F] {
-    // Cancels a Fiber waiting for the Mutex.
-    private def cancel(
-        thisCB: AsyncImpl.CB,
-        thisCell: AsyncImpl.LockCell,
-        previousCell: AsyncImpl.LockCell
-    ): F[Unit] =
-      F.delay {
-        // If we are canceled.
-        // First, we check if the state still contains ourselves,
-        // if that is the case, we swap it with the previousCell.
-        // This ensures any consequent attempt to acquire the Mutex
-        // will register its callback on the appropriate cell.
-        // Additionally, that confirms there is no Fiber
-        // currently waiting for us.
-        if (!state.compareAndSet(thisCell, previousCell)) {
-          // Otherwise,
-          // it means we have a Fiber waiting for us.
-          // Thus, we need to tell the previous cell
-          // to awake that Fiber instead.
+    override final val lock: Resource[F, Unit] =
+      Resource
+        .makeFull[F, Deferred[F, AsyncImpl.LockChain]] { poll =>
+          Deferred[F, AsyncImpl.LockChain].flatMap { lock =>
+            def loop(chain: AsyncImpl.LockChain): F[Deferred[F, AsyncImpl.LockChain]] =
+              chain match {
+                case AsyncImpl.Cons(otherLock) =>
+                  otherLock.asInstanceOf[Deferred[F, AsyncImpl.LockChain]].get.flatMap(loop)
 
-          // There is a tiny fraction of time when
-          // the next cell has acquired ourselves,
-          // but hasn't registered itself yet.
-          // Thus, we spin loop until that happens.
-          var nextCB = thisCell.get()
-          while (nextCB eq null) {
-            nextCB = thisCell.get()
-          }
+                case AsyncImpl.Empty =>
+                  F.pure(lock)
+              }
 
-          // Before telling previous to awake the next Fiber,
-          // We will set our cell in the terminal state (Sentinel),
-          // to signal that we are already completed.
-          // However, before doing that, we need to ensure our next callback,
-          // has not been concurrently modified.
-          while (!thisCell.compareAndSet(nextCB, AsyncImpl.Sentinel)) {
-            nextCB = thisCell.get()
-          }
-
-          // We are ready to tell the previous cell to awake the next Fiber in the chain.
-          if (!previousCell.compareAndSet(thisCB, nextCB)) {
-            // But, in case the previous cell had already completed,
-            // then the Mutex is free and we can awake our waiting Fiber.
-            if (nextCB ne null) nextCB.apply(Either.unit)
-          }
-        }
-      }
-
-    // Awaits until the Mutex is free.
-    private def await(thisCell: AsyncImpl.LockCell): F[Unit] =
-      F.asyncCheckAttempt[Unit] { thisCB =>
-        F.delay {
-          val previousCell = state.getAndSet(thisCell)
-
-          if (previousCell eq null) {
-            // If the previous cell was null,
-            // then the Mutex is free.
-            Either.unit
-          } else {
-            // Otherwise,
-            // we check again that the previous cell haven't been completed yet,
-            // if not we tell the previous cell to awake us when they finish.
-            if (!previousCell.compareAndSet(null, thisCB)) {
-              // If it was already completed,
-              // then the Mutex is free.
-              Either.unit
-            } else {
-              Left(Some(cancel(thisCB, thisCell, previousCell)))
+            state.getAndSet(AsyncImpl.Cons[F](lock)).flatMap { chain =>
+              F.onCancel(poll(loop(chain)), lock.complete(chain).void)
             }
           }
-        }
-      }
-
-    // Acquires the Mutex.
-    private def acquire(poll: Poll[F]): F[AsyncImpl.LockCell] =
-      F.delay(new AtomicReference[AsyncImpl.CB]()).flatMap { thisCell =>
-        poll(await(thisCell).map(_ => thisCell))
-      }
-
-    // Releases the Mutex.
-    private def release(thisCell: AsyncImpl.LockCell): F[Unit] =
-      F.delay {
-        // If the state still contains our own cell,
-        // then it means nobody was waiting for the Mutex,
-        // and thus it can be put on a free state again.
-        if (!state.compareAndSet(thisCell, null)) {
-          // Otherwise,
-          // our cell is probably not empty,
-          // we must awake whatever Fiber is waiting for us.
-          val nextCB = thisCell.getAndSet(AsyncImpl.Sentinel)
-          if (nextCB ne null) nextCB.apply(Either.unit)
-        }
-      }
-
-    override final val lock: Resource[F, Unit] =
-      Resource.makeFull[F, AsyncImpl.LockCell](acquire)(release).void
+        } { lock => lock.complete(AsyncImpl.Empty).void }
+        .void
 
     override def mapK[G[_]](f: F ~> G)(implicit G: MonadCancel[G, _]): Mutex[G] =
       new Mutex.TransformedMutex(this, f)
   }
 
   object AsyncImpl {
-    private[Mutex] type CB = Either[Throwable, Unit] => Unit
-    private[Mutex] final val Sentinel: CB = _ => ()
-    private[Mutex] type LockCell = AtomicReference[CB]
+    private[Mutex] sealed abstract class LockChain
+    private[Mutex] final case class Cons[F[_]](df: Deferred[F, LockChain]) extends LockChain
+    private[Mutex] case object Empty extends LockChain
   }
 
   private final class TransformedMutex[F[_], G[_]](
