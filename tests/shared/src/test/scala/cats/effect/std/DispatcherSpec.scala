393778d78f26f9719cad00e659e2eda25b3fe6cd
Daniel Spiewak <djspiewak@gmail.com>
2023-05-05 11:26:26 -0600

    Added test to ensure inner worker awaits are canceled

--- tests/shared/src/test/scala/cats/effect/std/DispatcherSpec.scala
+++ tests/shared/src/test/scala/cats/effect/std/DispatcherSpec.scala
@@ -152,6 +152,13 @@ class DispatcherSpec extends BaseSpec with DetectPlatform {
           _ <- gate2.acquireN(2)
         } yield ok
       }
+
+      "cancel inner awaits when canceled" in ticked { implicit ticker =>
+        val work = Dispatcher.parallel[IO](await = false).useForever
+        val test = work.background.use(_ => IO.sleep(100.millis))
+
+        test must completeAs(())
+      }
     }
   }
 
