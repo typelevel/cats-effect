fd9795e87f1bf44b4ed4f1e57e46694052b3043b
Daniel Spiewak <djspiewak@gmail.com>
2023-05-13 10:46:01 -0600

    Also create major merge PR

--- scripts/make-release-prs.sh
+++ scripts/make-release-prs.sh
@@ -14,6 +14,7 @@ old_version="$1"
 new_version="$2"
 
 minor_base=series/$(echo $new_version | sed -E 's/([0-9]+).([0-9]+).[0-9]+/\1.\2.x/')
+major_base=series/$(echo $new_version | sed -E 's/([0-9]+).[0-9]+.[0-9]+/\1.x/')
 branch="release/$new_version-minor"
 
 cd "$(mktemp -d)"
@@ -28,6 +29,14 @@ git push origin $branch
 gh pr create \
   --fill \
   --base $minor_base \
+  --repo typelevel/cats-effect \
+  --head typelevel:$branch \
+  --label ':robot:'
+
+gh pr create \
+  --title "Merge changes from $new_version into $major_base" \
+  --body '' \
+  --base $major_base \
   --repo typelevel/cats-effect \
   --head typelevel:$branch \
   --label ':robot:'
