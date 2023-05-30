b0bf31c94fd6c532b6c6e8c7019454826342ec00
Diogo Canut <diogocanut@hotmail.com>
2023-05-30 15:46:36 -0300

    avoid duplication of UUIDGen code

--- std/js-native/src/main/scala/cats/effect/std/UUIDGenCompanionPlatform.scala
+++ std/js-native/src/main/scala/cats/effect/std/UUIDGenCompanionPlatform.scala
@@ -28,38 +28,19 @@
 
 package cats.effect.std
 
+import cats.Applicative
 import cats.effect.kernel.Sync
-
-import java.util.UUID
+import cats.effect.std.Random.ScalaRandom
 
 private[std] trait UUIDGenCompanionPlatform extends UUIDGenCompanionPlatformLowPriority
 
 private[std] trait UUIDGenCompanionPlatformLowPriority {
+
+  private implicit def secureRandom[F[_]](implicit ev: Sync[F]) =
+    new ScalaRandom[F](Applicative[F].pure(new SecureRandom.JavaSecureRandom()))
+      with SecureRandom[F] {}
+
   implicit def fromSync[F[_]](implicit ev: Sync[F]): UUIDGen[F] =
-    new UUIDGen[F] {
-      private val csprng = new SecureRandom.JavaSecureRandom()
-      private val randomUUIDBuffer = new Array[Byte](16)
-      override final val randomUUID: F[UUID] =
-        ev.delay {
-          val buffer = randomUUIDBuffer // local copy
+    UUIDGen.fromSecureRandom[F]
 
-          /* We use nextBytes() because that is the primitive of most secure RNGs,
-           * and therefore it allows to perform a unique call to the underlying
-           * secure RNG.
-           */
-          csprng.nextBytes(randomUUIDBuffer)
-
-          @inline def intFromBuffer(i: Int): Int =
-            (buffer(i) << 24) | ((buffer(i + 1) & 0xff) << 16) | ((buffer(
-              i + 2) & 0xff) << 8) | (buffer(i + 3) & 0xff)
-
-          val i1 = intFromBuffer(0)
-          val i2 = (intFromBuffer(4) & ~0x0000f000) | 0x00004000
-          val i3 = (intFromBuffer(8) & ~0xc0000000) | 0x80000000
-          val i4 = intFromBuffer(12)
-          val msb = (i1.toLong << 32) | (i2.toLong & 0xffffffffL)
-          val lsb = (i3.toLong << 32) | (i4.toLong & 0xffffffffL)
-          new UUID(msb, lsb)
-        }
-    }
 }
