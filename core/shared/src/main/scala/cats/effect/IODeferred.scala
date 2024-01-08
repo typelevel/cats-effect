1d000764283a03fe8f061c880177c04dda1d1426
Arman Bilge <armanbilge@gmail.com>
2024-01-08 12:32:11 +0000

    Fix compile

--- core/shared/src/main/scala/cats/effect/IODeferred.scala
+++ core/shared/src/main/scala/cats/effect/IODeferred.scala
@@ -56,7 +56,7 @@ private final class IODeferred[A] extends Deferred[IO, A] {
   }
 
   private[this] val cell = new AtomicReference(initial)
-  private[this] val callbacks = CallbackStack[Right[Nothing, A]](null)
+  private[this] val callbacks = CallbackStack.of[Right[Nothing, A]](null)
   private[this] val clearCounter = new AtomicInteger
 
   def complete(a: A): IO[Boolean] = IO {
