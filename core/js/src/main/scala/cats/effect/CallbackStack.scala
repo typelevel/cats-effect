82c686f13674e56e34ab3a09f49e37387d3d0f7f
Arman Bilge <armanbilge@gmail.com>
2024-01-10 23:39:50 +0000

    Workaround weird unused warnings

--- core/js/src/main/scala/cats/effect/CallbackStack.scala
+++ core/js/src/main/scala/cats/effect/CallbackStack.scala
@@ -61,10 +61,8 @@ private final class CallbackStackOps[A](private val callbacks: js.Array[A => Uni
   @inline def clear(): Unit =
     callbacks.length = 0 // javascript is crazy!
 
-  @inline def pack(bound: Int): Int = {
-    val _ = bound
-    0
-  }
+  @inline def pack(bound: Int): Int =
+    bound - bound // aka 0, but so bound is not unused ...
 }
 
 private object CallbackStack {
