73315cb983dde8a777b3ccc28b643d2cca30567b
Arman Bilge <armanbilge@gmail.com>
2024-05-20 18:28:57 +0000

    More fixing

--- tests/shared/src/test/scala/cats/effect/IOSpec.scala
+++ tests/shared/src/test/scala/cats/effect/IOSpec.scala
@@ -1882,9 +1882,9 @@ class IOSpec extends BaseSpec with Discipline with IOPlatformSpecification {
 
       "timeoutAndForget" should {
         "terminate on an uncancelable fiber" in real {
-          IO.never.uncancelable.timeoutAndForget(1.second).attempt flatMap { e =>
+          IO.never.uncancelable.timeoutAndForget(1.second).attempt flatMap { r =>
             IO {
-              e must beLike { case Left(e) => e must haveClass[TimeoutException] }
+              r must beLike { case Left(e) => e must haveClass[TimeoutException] }
             }
           }
         }
